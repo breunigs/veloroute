36828d9b926469b792a510e0f09d5b3ed6dd1b38
Stefan Breunig <stefan-github@yrden.de>
2018-06-02 21:20:02 +0200

    paralellize

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,7 @@ cd $(dirname $0)
 sudo -u www-data git pull
 systemctl reload nginx
 
-sudo -u www-data ./update_relations.rb
-sudo -u www-data brotli -f --best *.js *.css *.html
-sudo -u www-data gzip -f -k --best *.js *.css *.html
+sudo -u www-data ./update_relations.rb&
+sudo -u www-data brotli -f --best *.js *.css *.html&
+sudo -u www-data gzip -f -k --best *.js *.css *.html&
+wait
