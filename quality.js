983cd0f9738e5168287d6ebe119362d95f1ce00f
Stefan Breunig <stefan-github@yrden.de>
2018-06-15 18:49:50 +0200

    understanding all tags is hard :(

--- quality.js
+++ quality.js
@@ -45,7 +45,7 @@ const quality = (function(map, index) {
     let value = tags[prefix + tag] || tags["cycleway_both_" + tag] || tags["cycleway_" + tag];
     // allow fallback for certain tag/value combinations
     let isCombinedFootCycleWay = kind == "track" && tags.highway == "footway";
-    let isOnStreet = kind == "lane" || kind == "opposite";
+    let isOnStreet = kind == "lane" || kind == "opposite" || kind == "opposite_lane" || prefix == "";
     if(isCombinedFootCycleWay || isOnStreet || tag == "lit") value = value || tags[tag];
     return value;
   }
