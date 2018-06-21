e0b8e7e20d4c52e9d60b37ae76b378f3217e36e0
Tobias Pickel <tobias.pickel@xing.com>
2018-06-21 15:58:58 +0200

    Add sass post css and extract css to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   devtool: "cheap-eval-source-map",
@@ -9,12 +10,29 @@ module.exports = {
     path: path.resolve(__dirname),
     filename: "bundle.js"
   },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      }
+    ]
+  },
   devServer: {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "base.css"
+    }),
     new HtmlWebpackPlugin({
       template: "main.html",
       minify: true,
