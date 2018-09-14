breunigs/veloroute

commit 062431aa506b48afd31aab88487aebbf58976e3c

fix route selection when in mobile view

src/images.js
@@ -1,6 +1,7 @@
 import { ImageSize, Viewer } from 'mapillary-js';
 import { readFromHash } from './state';
-import { toQualityName } from './utils_web';
+import { toQualityName, isMobileView } from './utils_web';
+
 import filenames from '../routes/geo/content_hashed_filenames.json';
 
 const API_KEY = 'MjFBX2pVMXN2aUlrSXFCVGlYMi11dzo4Yjk0NGY1MjMzYmExMzI2';
@@ -113,7 +114,9 @@ const startPlaybackWithDefaultBranch = async (routeName) => {
   status.routeName = routeName;
   // TODO: maybe keep current branch if it's part of the route?
   status.branch = null;
-  maybeSelectDefaultBranch().then(() => handlePlay());
+  const promise = maybeSelectDefaultBranch()
+  // load Mapillary, but do not start playback on mobile, as it's below the fold
+  if(!isMobileView(body)) promise.then(() => handlePlay());
 }
 
 async function setActiveRoute(routeName, branch, imgIndex, manualInteraction) {

src/index.js
@@ -1,7 +1,6 @@
 import "../base.scss";
 
 import { map, addRouteClickListener, addQualityClickListener, renderIndicator, toggleQuality, createMarker } from "./map";
-import { isMobileView } from './utils_web';
 import AbstractRoute from "./abstract_route";
 import places from '../routes/geo/places.json';
 import State from './state';
@@ -71,7 +70,7 @@ document.addEventListener('click', ev => {
   if(path.match(/^(quality|quality\/[0-9a-z-]+|\d|1[01234])$/) || path === "") {
     ev.preventDefault();
     state.routeSetter()(path);
-    if(anchor.classList.contains('autoplay') && !isMobileView(body)) {
+    if(anchor.classList.contains('autoplay')) {
       imagesPromise.then(({startPlaybackWithDefaultBranch}) => {
         startPlaybackWithDefaultBranch(path);
       });

commit f0ce51044e3897a94cc5f44c1ec7e0af829a2de1

fix autoplay broken in 062431aa506b48afd31aab88487aebbf58976e3c

src/abstract_route.js
@@ -42,7 +42,7 @@ class AbstractRoute {
       }
     }
 
-    if(isMobileView()) {
+    if(isMobileView(body)) {
       const rectBefore = scrollbox.getBoundingClientRect();
       const routesBottomVisible = rectBefore.bottom-rectBefore.height < 0;
 

src/images.js
@@ -12,6 +12,7 @@ const next = document.getElementById("next");
 const direction = document.getElementById("direction");
 const playstop = document.getElementById("playstop");
 const controls = document.getElementById("controls");
+const body = document.getElementsByTagName('body')[0];
 
 const GENERIC_START_IMAGE = 'c4B6txFX6Xgza8iWNFzSYw';
 