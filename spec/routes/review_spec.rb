2240b2d1ed055bb207345537aea82d0d6b565bdd
Stefan Breunig <stefan-github@yrden.de>
2018-09-23 12:57:51 +0200

    stub places properly

--- spec/routes/review_spec.rb
+++ spec/routes/review_spec.rb
@@ -8,7 +8,7 @@ describe Review do
 
   before do
     stub_request(:get, %r|https://nominatim.openstreetmap.org/search/|).
-      to_return(status: 200, body: "{}", headers: {})
+      to_return(status: 200, body: %|[{"place_id":"198093421","licence":"Data © OpenStreetMap contributors, ODbL 1.0. https:\/\/osm.org\/copyright","osm_type":"relation","osm_id":"456925","boundingbox":["53.5501228","53.5515159","9.9918396","9.9943706"],"lat":"53.55081605","lon":"9.99289936180208","display_name":"Rathausmarkt, Hamburg-Altstadt, Hamburg-Mitte, Hamburg, 20095, Deutschland","class":"highway","type":"pedestrian","importance":0.48123930027933803}]|, headers: {})
   end
 
   describe '.mentioned_places' do
--- spec/routes/route_spec.rb
+++ spec/routes/route_spec.rb
@@ -33,7 +33,7 @@ describe Route, type: :model do
 
   before do
     stub_request(:get, %r|https://nominatim.openstreetmap.org/search/|).
-      to_return(status: 200, body: "{}", headers: {})
+      to_return(status: 200, body: %|[{"place_id":"198093421","licence":"Data © OpenStreetMap contributors, ODbL 1.0. https:\/\/osm.org\/copyright","osm_type":"relation","osm_id":"456925","boundingbox":["53.5501228","53.5515159","9.9918396","9.9943706"],"lat":"53.55081605","lon":"9.99289936180208","display_name":"Rathausmarkt, Hamburg-Altstadt, Hamburg-Mitte, Hamburg, 20095, Deutschland","class":"highway","type":"pedestrian","importance":0.48123930027933803}]|, headers: {})
   end
 
   it "only splits routes when necessary" do
