ad2402b64482dc12ddc8ecc0b23d966dd1430bb0
Stefan Breunig <stefan-github@yrden.de>
2020-06-07 22:07:49 +0200

    fix build(?)

--- lib/bounding_box.ex
+++ lib/bounding_box.ex
@@ -1,11 +1,3 @@
-defimpl Phoenix.HTML.Safe, for: BoundingBox do
-  def to_iodata(bounds) do
-    bounds
-    |> VelorouteWeb.VariousHelpers.to_string_bounds()
-    |> Plug.HTML.html_escape_to_iodata()
-  end
-end
-
 defmodule BoundingBox do
   @params [:minLon, :minLat, :maxLon, :maxLat]
   @enforce_keys @params
---
+++ lib/veloroute_web/views/bounding_box_safe.ex
@@ -0,0 +1,7 @@
+defimpl Phoenix.HTML.Safe, for: BoundingBox do
+  def to_iodata(bounds) do
+    bounds
+    |> VelorouteWeb.VariousHelpers.to_string_bounds()
+    |> Plug.HTML.html_escape_to_iodata()
+  end
+end
