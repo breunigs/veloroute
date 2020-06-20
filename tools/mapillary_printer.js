f64599569e7e2d26faee67e4dd57a39c1586ef5c
Stefan Breunig <stefan-github@yrden.de>
2020-06-20 11:00:38 +0200

    different workaround in mly printer againt incorrect sequence Ids

--- tools/mapillary_printer.js
+++ tools/mapillary_printer.js
@@ -1,22 +1,22 @@
 let prevKey = null;
-let leTimeout = null;
 
 function printStuffs() {
   let pKey = history.state.pKey;
   if (prevKey == pKey) return;
-  document.querySelector(".mapboxgl-ctrl.mapboxgl-ctrl-attrib").innerHTML = '<b><tt>- { seq: "<span style="user-select: all" id="seqkey">??????????????????????</span>", from: "<span style="user-select: all">' + pKey + '</span>", to: "<span style="user-select: all">' + pKey + '</span>" }</tt></b>';
   prevKey = pKey;
-  console.log(pKey);
-  if (leTimeout) { clearTimeout(leTimeout); }
-  leTimeout = setTimeout(fetchStuffs, 500);
-}
 
-function fetchStuffs() {
   document.getElementById("OptionsIconContainer").click();
   let seq = document.querySelectorAll("app-copy-field input")[1].value
   document.getElementById("OptionsIconContainer").click();
-  document.querySelector("#seqkey").innerHTML = seq;
+
+  document.querySelector(".mapboxgl-ctrl.mapboxgl-ctrl-attrib").innerHTML = '<b><tt>- { seq: "<span style="user-select: all" id="seqkey">' + seq + '</span>", from: "<span style="user-select: all">' + pKey + '</span>", to: "<span style="user-select: all">' + pKey + '</span>" }</tt></b>';
+
+  // fix wrong sequence key
+  setTimeout(function () {
+    prevKey = null;
+    printStuffs();
+  }, 1000);
 }
 
-window.setInterval(printStuffs, 10);
+window.setInterval(printStuffs, 100);
 printStuffs();
