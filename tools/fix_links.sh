9bd1121470e39eb2e47066e20baaf2f40c8d2fff
Stefan Breunig <stefan-github@yrden.de>
2020-12-11 13:35:19 +0100

    enhance auto link fixer

--- tools/fix_links.sh
+++ tools/fix_links.sh
@@ -3,3 +3,4 @@
 cd "$(dirname "$(readlink -f "$0")")"
 cd ..
 sed --regexp-extended -i 's| (http[^ ]+)?bounds=([0-9.]+)%2C([0-9.]+)%2C([0-9.]+)%2C([0-9.]+)&img=([a-z0-9A-Z_-]{22})| bounds="\2,\3,\4,\5" img="\6"|g' data/articles/*.yaml
+sed --regexp-extended -i 's| (http[^ ]+)?bounds=([0-9.]+)%2C([0-9.]+)%2C([0-9.]+)%2C([0-9.]+)| bounds="\2,\3,\4,\5"|g' data/articles/*.yaml
