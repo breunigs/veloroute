4b73952856f8807437e978318b4953d635613268
Stefan Breunig <stefan-github@yrden.de>
2021-02-19 22:48:41 +0100

    fix link fix (what irony)

--- tools/fix_links.sh
+++ tools/fix_links.sh
@@ -1,6 +1,7 @@
-#!/bin/bash
+#!/usr/bin/zsh
 
 cd "$(dirname "$(readlink -f "$0")")"
 cd ..
+pwd
 sed --regexp-extended -i 's| (http[^ ]+)?bounds=([0-9.]+)%2C([0-9.]+)%2C([0-9.]+)%2C([0-9.]+)&img=([a-z0-9A-Z_-]{22})| bounds="\2,\3,\4,\5" img="\6"|g' data/articles/**/*.yaml
 sed --regexp-extended -i 's| (http[^ ]+)?bounds=([0-9.]+)%2C([0-9.]+)%2C([0-9.]+)%2C([0-9.]+)| bounds="\2,\3,\4,\5"|g' data/articles/**/*.yaml
