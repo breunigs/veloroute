8eef1cc3cf99783f0dd54695fa1fd2452616b1d7
Stefan Breunig <stefan-github@yrden.de>
2021-04-05 14:45:20 +0200

    convert route 8 to new map.osm style

--- data/route/route8.ex
+++ data/route/route8.ex
@@ -1,46 +1,4 @@
 defmodule Data.Route.Route8 do
   use Data.Route,
-    yaml: """
-    # 2020
-    8 (stadtauswärts nach Bergedorf):
-    - { seq: "RZsSKCpUFcglnuQntM19eA", from: "lcGaLJMcKAWmgUo2ECz9iA", to: "0CvjzdF-8tryZWZpdr1cCA" }
-    - { seq: "RZsSKCpUFcglnuQntM19eA", from: "sYNg443OwuGk7YBlfvtRIg", to: "C9K0pwIgmoWl6d4G16kTyg" }
-    - { seq: "BdDzOl_qNFtvFujX3GcAjQ", from: "WPP8F-UnfGZsY4Lh2w8mKQ", to: "-TE45g6EFHXp85pG2QaCFA" }
-    - { seq: "cEEAqABQHfrrXDLmuufvpA", from: "5Q-3m0Uuh7JHddVbWu_f2w", to: "rbKzYoytA0HqyAKBA4NmXA" }
-    - { seq: "sgwq3-AJka6lwgihNKHbQg", from: "s6rrTavg9Nxe9LiQRi5zeQ", to: "IRsMe59Tc8PfI-FCkAnh8g" }
-    - { seq: "LRXFJ4jo2YEWHc0Efu0Chg", from: "_55pou7WdRqyKMSSjdoJvw", to: "CzoeZuYG0krjQf4RyF5q3A" }
-    - { seq: "0wDg8K2zPRU6rzax3yFzBw", from: "kGPtXiHWAA1VJatToNvFmw", to: "zb0Vv404E0SbJyUrMfHCDA" }
-    - { seq: "0wDg8K2zPRU6rzax3yFzBw", from: "qPCU9vZpkE0ixHBlzjCSRg", to: "12gMKcuF1p9VioKSvVL_xw" }
-    - { seq: "gGtaKeqV7v6jtX3sCcz_oQ", from: "VP5NoEj94swc9OPClWX3Tg", to: "qeVDQ3cpffQHauzOf0wpyg" }
-    - { seq: "57409kVKBaffQvurHxNgtw", from: "DH88PCLRsk-YR3DncvM_tw", to: "scKUuevEch1hxbQ4T02ptg" }
-    - { seq: "_8HiS-M6LPamR_TfwYDLxA", from: "8lFL4eQ6QN5tuQT36hGRmQ", to: "NCKw4KDzhlIPLXIx4AJFFA" }
-    - { seq: "1tOdAnNkuPR6vK2u_wulrA", from: "KGmAD3R3IDTJ7R8TNQ4f8w", to: "fE6Nj7bluPr9JYv4bkVwog" }
-
-    8 (stadteinwärts von Bergedorf):
-    - { seq: "1tOdAnNkuPR6vK2u_wulrA", from: "lITueAkbVljBbrHI3LploA", to: "tSj9YP9JdUoAvrgPCDFokQ" }
-    - { seq: "Zc9Nduc1Aih_9xaANU3-lg", from: "xGLedZSa4ltJ511ACUw48A", to: "kGExHI5mrNlhyD30uoF92g" }
-    - { seq: "stJBIK-_SbETaUzywpDOOw", from: "aFp4KqIM2rvo5s344ql7RA", to: "bShs_mf_-x_ERyXsORyZQQ" }
-    - { seq: "AEWwZr4TiLvJxZrHB4gklA", from: "08wzEsVEeaAtNw41ZEZvWA", to: "-q9RnZplF1meUnG9zRMvrw" }
-    - { seq: "2fEwkC94NPNbBhvYziLt7w", from: "RtRnrllbQMkXE7APDLpAlA", to: "tFdA6jEB9coBJOKLmqyLzg" }
-    - { seq: "HfmBAatta51ldKOdOVDk0A", from: "rtyvq3lFKK0jdspdMEL-JA", to: "-xaavnrp0MFSXJoF92UzUA" }
-    - { seq: "HfmBAatta51ldKOdOVDk0A", from: "lro0sXud22nzfepie1S19A", to: "AI4G-NRsOPpsEn1cEyE73Q" }
-    - { seq: "XpNK5nkZsCyKLMLh6E6F1w", from: "QJcasrJytWodqpc6qDE1PA", to: "YmQcJIHu0SoE1TNdhiOleg" }
-    - { seq: "DyEc5CjxNtGuMsDf_CPGcg", from: "U4p7yBc2Rmj6AqG9he28Ew", to: "Hv2wuojS4yT17CUu4o92hw" }
-    - { seq: "DyEc5CjxNtGuMsDf_CPGcg", from: "i6wOlJDlQq6BT_P3KklL7Q", to: "Bhc6Ad_5OyLs0PN7vfruOg" }
-    - { seq: "ei1HHyLTbVrWiDPOk0KdPA", from: "kpWKvtkKdrcSJzhLEtWaeA", to: "WWJDt1-qje46KAMy4LUYpQ" }
-    - { seq: "sJPiXwRLqZQeupW0G8-SMQ", from: "vwnY_qcPDGikHZddzG78Rg", to: "HTHHPLyCiv3q0-u8Lr2SsQ" }
-    - { seq: "AZ2uLAkjg6jB6VAM-oXn0g", from: "Kt-oOhX3B3gK90UDMZwpzA", to: "fap5vI5Z8W_pQ-KPcPnEkg" }
-    - { seq: "2N7ydegL5XD6Fh12UIRFfA", from: "x16vJoqVRYmxmhOpImivHw", to: "xrEvGbpuPtSakwfLKYj5Rg" }
-    - { seq: "CI5s5YQOVVLy9FQVDUnsOg", from: "26jP0zzVy4gQK-clCjrEJQ", to: "SyqIleMIdQjhq9cghz1ZnQ" }
-    - { seq: "QSl3dQRNQU-HlZtMowf_Sg", from: "P6V9RUTo4mwBUetjGHS4Ew", to: "UPC1T9nXYnqOymAjcdd2uw" }
-
-
-    # 2019
-    8 (stadtauswärts zum Mümmelmannsberg):
-    - { seq: "MZqzQJU_KK41nKLdH3xXrQ", from: "SaBlbYKYlLnObHAfvGGPmA", to: "noJA9nXcalCYeL8KrwE9Ew" }
-    - { seq: "6N_C-gUg3HwXywoaexPG2A", from: "SIoeOMQGadQc5uODCF32Kw", to: "wcrmLt4uyvTdgR91vUEKLw" }
-    8 (stadteinwärts vom Mümmelmannsberg):
-    - { seq: "6N_C-gUg3HwXywoaexPG2A", from: "wcrmLt4uyvTdgR91vUEKLw", to: "n-Wp7-W0StUgIkbE9YbH1A" }
-    - { seq: "LIbLx-LVizt4iXrBp-RGKw", from: "EL-70gnay8TC8N2IANao0A", to: "ILVogC844T8CM5YJ5VAcPw" }
-    """
+    id: "8"
 end
