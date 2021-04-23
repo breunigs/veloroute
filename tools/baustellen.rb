bb99a684201c894dc7631f4727878bcd93e5399e
Stefan Breunig <stefan-github@yrden.de>
2021-04-23 19:56:56 +0200

    improve construction date support with multiple entries

--- tools/baustellen.rb
+++ tools/baustellen.rb
@@ -9,14 +9,6 @@ require 'parallel'
 
 SEEN_FN = File.join(__dir__, './baustellen_seen_ids.txt')
 seen = YAML.load_file(SEEN_FN)
-def ids_by_date(date)
-  json = OpenURI.open_uri("https://bauweiser.hamburg.de/api/steckbriefeweb/list?date=#{date}").read
-  print "."
-  JSON.parse(json).map { |entry| entry["id"] }
-end
-
-ids = ids_by_date(Date.today) + ids_by_date(Date.today + 7) + ids_by_date(Date.today + 30)
-ids = ids.uniq
 
 references = Dir.glob(File.join(__dir__, "../data/articles/*.yaml")).flat_map do |path|
   construction_ids = YAML.load_file(path)["construction_site_id_hh"]
@@ -24,15 +16,20 @@ references = Dir.glob(File.join(__dir__, "../data/articles/*.yaml")).flat_map do
 end.to_h
 references.delete(nil)
 
+def ids_by_date(date)
+  json = OpenURI.open_uri("https://bauweiser.hamburg.de/api/steckbriefeweb/list?date=#{date}").read
+  print "."
+  JSON.parse(json).map { |entry| entry["id"] }
+end
+
+ids = ids_by_date(Date.today) + ids_by_date(Date.today + 7) + ids_by_date(Date.today + 30)
+ids = (ids + references.keys).uniq
+
+
 updates = Parallel.map(ids, in_threads: 4) do |id|
   jsonRaw = OpenURI.open_uri("https://bauweiser.hamburg.de/api/steckbriefeweb/id/#{id}").read
   json = JSON.parse(jsonRaw)
   print "."
-
-  prev = seen[json["id"]]
-  if prev && prev[:updated] == json['updateDatetime']
-    next
-  end
 
   {
     id: id,
@@ -45,31 +42,22 @@ updates = Parallel.map(ids, in_threads: 4) do |id|
   }.compact
 end.compact
 
-updates.each do |upd|
-  seen[upd[:id]] = upd
 
+rewrite = {}
+
+updates.each do |upd|
   path = references[upd[:id]]
   if path
-    startText = "start: #{upd[:start]}"
-    endText = "end: #{upd[:end]}"
-    full = File.read(path)
-    updated = full.dup
-
-    if !updated.include?(startText) && upd[:start]
-      updated.sub!(/^start: .*$/, startText) || updated.prepend("#{startText}\n")
-    end
-    if !updated.include?(endText) && upd[:end]
-      updated.sub!(/^end: .*$/, endText) || updated.prepend("#{endText}\n")
-    end
-
-    if full != updated
-      File.write(path, updated)
-      puts "\n✓ #{File.basename(path)}"
-    end
-
-    next
+    rewrite[path] ||= {starts: [], ends: []}
+    rewrite[path][:starts] << upd[:start]
+    rewrite[path][:ends] << upd[:end]
   end
 
+  prev = seen[upd[:id]]
+  alreadySeen = prev && prev[:updated] == upd[:updated]
+  next if alreadySeen
+
+  seen[upd[:id]] = upd
   puts <<~TEXT
 
     #{upd[:title]}
@@ -80,6 +68,27 @@ updates.each do |upd|
   TEXT
   puts "  " + upd[:link] if upd[:link]
 end
+
+rewrite.each do |path, dates|
+  startDate = dates[:starts].compact.min
+  endDate = dates[:ends].compact.max
+  startText = "start: #{startDate}"
+  endText = "end: #{endDate}"
+
+  updated = File.read(path)
+  next if updated.include?(startText) && updated.include?(endText)
+
+  if startDate
+    updated.sub!(/^start: .*$/, startText) || updated.prepend("#{startText}\n")
+  end
+  if endDate
+    updated.sub!(/^end: .*$/, endText) || updated.prepend("#{endText}\n")
+  end
+
+  File.write(path, updated)
+  puts "\n✓ #{File.basename(path)}"
+end
+
 
 File.write(SEEN_FN, seen.to_yaml)
 puts
