8fadfbfe44e6f98c3b0e7939220c03983f20a346
Stefan Breunig <stefan-github@yrden.de>
2021-07-13 17:57:52 +0200

    migrate route5 to video

--- data/route/route5.ex
+++ data/route/route5.ex
@@ -1,68 +1,123 @@
 defmodule Data.Route.Route5 do
-  use Route.Parser,
-    yaml: """
-    5 (stadtauswärts nach Duvenstedt):
-    - { seq: "8c1_G8OslL0aGG6la2r_xw", from: "pv29Z5VfMV7N6qjcu_nC6Q", to: "WtjMdwHeFpasucW43irpbw" }
-    - { seq: "6YoTITDUhO_huqjbNlx4PA", from: "VooCrNV8V6H-ZmPvXZ1Qfg", to: "O37y9br9BcfwCVBkfhoB2Q" }
-    - { seq: "Uaof0k_sR36e9ttS-6cS2g", from: "uGIl7i_ita63BddzDLNGXA", to: "yRT54e96X8SnNYNH_faovg" }
-    - { seq: "Uaof0k_sR36e9ttS-6cS2g", from: "VaYhgR8LyWEWsVgHAR3-3g", to: "mpM20ISUwf-ErWrKJh1SIw" }
-    - { seq: "EAnxTNSu2L7uy_IrtSgfQQ", from: "GLNtfeWxuySdKiiAVDG9sw", to: "IMEsY7V2_kLFyhNIG8m3dw" }
-    - { seq: "2tugNV7ZSZ70Xfqc_An5iA", from: "XDKbQiuYYl7S0yenO8zKyw", to: "wociVzEHqrSnr1xP8WeKug" }
-    - { seq: "DdXQLEoZCC_vZwplPVx09w", from: "_xCXVjvL2W2uwmjuf-oTbQ", to: "oDf-cxRgcoQnvWaPyHy8pg" }
-    - { seq: "DdXQLEoZCC_vZwplPVx09w", from: "1iNgVr0udmoFCwu0FuszZw", to: "JgbnlFXrAMRezPhkG1Ac0Q" }
-    - { seq: "rv2rUx0icLh1hZs3Vb7isw", from: "3_ganXg9dZFmc8W1oK9IdQ", to: "6bHZzoTlsacX65wxXHpHhg" }
-    - { seq: "OCWTuLU2BO3CkOntLyKGpA", from: "CaoGki1SurN7j1pty4cYEQ", to: "i5ecv-z4K7fxO3PYITYESg" }
-    - { seq: "OCWTuLU2BO3CkOntLyKGpA", from: "wmRbrP9JHJjiMmvaxPG9Tw", to: "fzWN1zvP2p_7qgCm2NkVDQ" }
-    - { seq: "Ew0mzLCnPZhsmjL0Cc62Mg", from: "G2GUPECjo-aX5M-EAOyxlg", to: "QvguD0dDMeZfhon0S3bLMw" }
-    - { seq: "Ew0mzLCnPZhsmjL0Cc62Mg", from: "ULcbgveD5_RUELQghYaNwA", to: "omv_RmZ6xc0rXPBFGK5_1A" }
-    - { seq: "2dSgocS1xYYKSNTVoHplyw", from: "60Fp-LIJIGIV9kjI7enKTA", to: "CUkYpEyNNTOP3KAQ5cNFYg" }
-    - { seq: "KE5H4fNAFRktrxp6gFRfxw", from: "d5IlpJ6kBOyT19JOl8kDDA", to: "gILbIURpPVGsO7b_juK0aQ" }
-    - { seq: "6LmM59ijOVVoY_QqtNGyBg", from: "v86Iy5u62cK_2q6wWAELQA", to: "a6fEZtd3yEEMsmoon88kAw" }
-    - { seq: "qcmL5P8eTZd61kakBquojw", from: "pO0sh6S62HVrHMrifGywGg", to: "D-wMGf1NIIJh3xH_Gvy9pw" }
-    - { seq: "6LmM59ijOVVoY_QqtNGyBg", from: "gmBJq7pUzAwTRrNbK6yLaA", to: "Dk4pFZ_KpLrBBfJGzwHDBA" }
-    - { seq: "GekesR84HVV3KDlao--lUQ", from: "Zym3ssECSMJCYwxe2qRdBw", to: "0YQKV-jtR02m252a3VDMXQ" }
-    - { seq: "2024meMJgNTSYvnM56RmBw", from: "UgyPLcheMdyq6U2tl6TBsQ", to: "_wsj0IJdXNCrC189ueBEBg" }
-    - { seq: "Oj8qEJNtzKasMn7xVVluzw", from: "PX_JZY35Jl0Soe-yDm0W7Q", to: "1Jeq9x5HLTFxenSUEPvAwQ" }
+  @behaviour Route.Behaviour
 
-    5 (stadteinwärts von Duvenstedt):
-    - { seq: "Oj8qEJNtzKasMn7xVVluzw", from: "60fBl2EHEQTEiEZgQSjKjw", to: "0iPIVIhrRWlIIWMELNRaLA" }
-    - { seq: "1rX26d1xVnjn24__-fklqQ", from: "mhamxSesyqgOl6DLAt5zsA", to: "jM57N01iWVJwIspME1cTiA" }
-    - { seq: "otaUS6TVadgf2INu3mJlTw", from: "ibjkE9CIkAmP8RjrgF3GXA", to: "CncLEI1dkSIyCsVA5VBiPg" }
-    - { seq: "V3egkufEH9QYon-B2NQSVA", from: "clxbvTJwvrvWzSKh6Wot9w", to: "BXf7eU2iFuMfaAWWdKkRjQ" }
-    - { seq: "nYtvXZwyomqMhbi4DGHqCQ", from: "chMP4fWAOzScNfD9nKO0KA", to: "T7KwxPGVH8RyNaH1jORIMw" }
-    - { seq: "qcmL5P8eTZd61kakBquojw", from: "hpzRIRBFLApG4rfOZvsPkQ", to: "4iBlJGMDymZ8ly9sxoQrSQ" }
-    - { seq: "qcmL5P8eTZd61kakBquojw", from: "g_ClKx5eift0rpWE-UJCTw", to: "MRJ9s2tcODAOYIOLybGH7A" }
-    - { seq: "V3egkufEH9QYon-B2NQSVA", from: "FLcknMEm1hOdTeO8sJRk_w", to: "TVy0PuA5LkdDZHF5NYyLMQ" }
-    - { seq: "qHm54F-s7VdiV1O9njWLrw", from: "uYZbSNOGSw4aCVsk6MSv2Q", to: "rn5RXydoCr2vuThldISHfg" }
-    - { seq: "Ew0mzLCnPZhsmjL0Cc62Mg", from: "dkvEtV_dUxto_KQaRZ-LuA", to: "YyVB9dga1nBBNb5VIXm4mw" }
-    - { seq: "vbxoyklPcv31CYK04wkgnA", from: "RM8QqpEmwx1w03CxC2c0Uw", to: "605sARUnys0NxUdQhpLa9Q" }
-    - { seq: "GrrdaocpAzVEXcUT1odz1g", from: "Roxq3YlHHqkK8SzwHXsKTA", to: "Vz_ywUus97gzmTNzDU7aAQ" }
-    - { seq: "kfQGYhIQ7hpeZyRBXikHhA", from: "QS1QxQfdfBwPob2BPTCbDQ", to: "E6Vn0bEKfXQ1ecYKhmhIQg" }
-    - { seq: "tkRn4R1MksnZPHljYkDnTA", from: "6I5taN3DOc6ya40aNVLnEA", to: "POA-rAdJChzhrJRqrThmrw" }
-    - { seq: "WRstOUJ3fyOjA-cP3RDmGw", from: "2f9MQc9j9VRd4rLXnm_9ww", to: "I3Oiz80-UnRgpPJRd0z05A" }
-    - { seq: "iDxNYlQitoX8djoMeZG7LQ", from: "6ApYc_eApZ9cWfpXkEI07w", to: "JmZpfA1qeQgxQ5ZB7wKBKQ" }
-    - { seq: "vfF4HubNTdLTF2xaPILhAA", from: "MZtyFioo4fvr4FhgfHn44A", to: "HI1ZamWB5tqD6rATychtmQ" }
-    - { seq: "TYtOnj73gXuYzZmJvaHTnQ", from: "6cq5PHmksV6OJP1JNJ5-kg", to: "9BdkNzOurOOT7Ev-q96Ijg" }
+  def id(), do: "5"
+  def color(), do: "#078a3d"
+  def name(), do: "Alltagsroute 5"
+  def osm_relation_ref(), do: "https://www.openstreetmap.org/relation/226032"
 
+  forward = %Video.Track{
+    group: "5",
+    direction: :forward,
+    from: "Innenstadt",
+    to: "Duvenstedt",
+    parent_ref: __MODULE__,
+    text: "aus der Innenstadt nach Duvenstedt",
+    videos: [
+      {"2021-06-22-velo7-und-pauli-altona/GX011937", "0:00:45.812", "0:00:48.682"},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011826", "0:00:15.782", :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011827", :start, "0:00:22.389"},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011779", "0:00:00.834", :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011780", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011781", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011782", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011783", :start, "0:00:01.502"},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011796", "0:00:00.567", :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011797", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011798", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011799", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011800", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011801", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011802", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011803", :start, "0:01:25.185"},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011804", "0:00:00.801", :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011805", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011807", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011808", :start, "0:02:45.766"},
+      {"2021-05-29-freizeitroute1/GX011488", "0:04:12.519", :end},
+      {"2021-05-29-freizeitroute1/GX021488", :start, "0:00:31.465"}
+    ]
+  }
 
-    5N (stadtauswärts zum Flughafen):
-    - { seq: "kfQGYhIQ7hpeZyRBXikHhA", from: "q6_J16pgKrV5EbQ7EHXWBw", to: "d6MhCh65mey5oigz6Kd1XA" }
-    - { seq: "GrrdaocpAzVEXcUT1odz1g", from: "45YNZ61pEAYPtc2G421XQg", to: "reMrMkT2VWzW05W-ysruzw" }
-    - { seq: "-GAR0Q33pvBO714AJ4t9lg", from: "riGZ2nq4WfKjm5aNzGT19A", to: "2S68GPwbgdoU0BNBeuS44A" }
-    - { seq: "-GAR0Q33pvBO714AJ4t9lg", from: "1KyW6-R5NNXSbXvMyPD3mQ", to: "X1jDBX68G5KySeJNp6-EiA" }
-    - { seq: "-GAR0Q33pvBO714AJ4t9lg", from: "Fi6TmLsARN-6tlWoySZ1oQ", to: "hyIt2UpgN5YzkCpE6os37g" }
-    - { seq: "-GAR0Q33pvBO714AJ4t9lg", from: "PklhNZqyr5JeyhYMWQYumQ", to: "-dlC-7oy1YVIJ8ApR_Z3MQ" }
-    - { seq: "ZMcqLcdtugUfGYiBmIB8hQ", from: "haiZ4-wfkLZcTWISc7xWZA", to: "hYJNuOI4cf16s9CbaU-EjA" }
-    - { seq: "ZMcqLcdtugUfGYiBmIB8hQ", from: "KCd_FXVWUyF5AZTBIt9sJA", to: "CEEV5e7XDuhBGsfOJmuHLQ" }
-    - { seq: "ZMcqLcdtugUfGYiBmIB8hQ", from: "vkhZsGae9T6m2sTuZgMNrQ", to: "QZ-Vp_QtUm4Jh36aluV3Zw" }
+  backward = %Video.Track{
+    group: "5",
+    direction: :backward,
+    from: "Duvenstedt",
+    to: "Innenstadt",
+    parent_ref: __MODULE__,
+    text: "von Duvenstedt in die Innenstadt",
+    videos: [
+      {"2021-05-29-freizeitroute1/GX021488", "0:00:32.399", "0:00:55.055"},
+      {"2021-05-29-freizeitroute1/GX011488", "0:00:29.062", "0:01:41.868"},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011808", "0:02:57.444", :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX021808", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011809", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011810", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011811", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011812", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011813", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011814", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011815", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011816", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011817", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011818", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011819", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011820", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011821", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011822", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011823", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011824", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011825", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011829", "0:00:13.947", :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011830", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011831", :start, "0:00:02.336"},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011826", "0:00:09.610", "0:00:14.348"}
+    ]
+  }
 
-    5N (stadteinwärts vom Flughafen):
-    - { seq: "ZMcqLcdtugUfGYiBmIB8hQ", from: "QZ-Vp_QtUm4Jh36aluV3Zw", to: "DQ5ibr6fnZWBRtMWojlXrQ" }
-    - { seq: "ZMcqLcdtugUfGYiBmIB8hQ", from: "I1jyb7nTM2y7HlE15PouJQ", to: "OKlg47Sj_-oYQ2upSl9JXA" }
-    - { seq: "7ccwSNzXO-kJOrWrJbkhrg", from: "lpgzvOPuLY67ZVxUAHdp-A", to: "bhRiVWIKFE_-PwXPq22EOg" }
-    - { seq: "bDnZGjzkMpcls9fTEPFw8w", from: "Imatgv6-WuDxIQS7b8izMg", to: "2WuF9cxfxqsk_ajQKJi6vg" }
-    - { seq: "tkRn4R1MksnZPHljYkDnTA", from: "6GKHqfjHbctrPsPyg-gRwg", to: "10jMUOYH51oTZNdnZnUsrw" }
-    - { seq: "YOIEftG1ggGYRF1kWXF1xw", from: "EGCngmozby3CPochnniyTQ", to: "q7HOqfxcvCHLa7iIAY1R8A" }
+  forward_5N = %Video.Track{
+    group: "5N",
+    direction: :forward,
+    from: "Hamburger Meile",
+    to: "City Nord",
+    parent_ref: __MODULE__,
+    text: "Hamburger Meile zur City Nord",
+    videos: [
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011783", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011784", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011785", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011786", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011787", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011788", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011789", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011790", :start, :end}
+    ]
+  }
 
-    """
+  backward_5N = %Video.Track{
+    group: "5N",
+    direction: :backward,
+    from: "City Nord",
+    to: "Hamburger Meile",
+    parent_ref: __MODULE__,
+    text: "City Nord zur Hamburger Meile",
+    videos: [
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011792", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011793", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011794", :start, :end},
+      {"2021-06-13-veloroute-5-5a-und-13rein/GX011795", :start, :end}
+    ]
+  }
+
+  @tracks [forward, backward, forward_5N, backward_5N]
+  def tracks(), do: @tracks
+
+  # legacy
+  def parsed() do
+    %{{id(), ""} => []}
+  end
+
+  def index() do
+    %{}
+  end
 end
