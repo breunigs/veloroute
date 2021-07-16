398aec2b6b18c2ab593f2057ef44c4034e301556
Stefan Breunig <stefan-github@yrden.de>
2021-07-16 08:47:46 +0200

    convert route7

--- data/route/route7.ex
+++ data/route/route7.ex
@@ -1,53 +1,105 @@
 defmodule Data.Route.Route7 do
-  use Route.Parser,
-    yaml: """
-    # 2020
-    7 (stadtauswärts nach Rahlstedt):
-    - { seq: "RZsSKCpUFcglnuQntM19eA", from: "qmoVJ60zQJ2RLyFyHvkB_g", to: "dJYcd04kxXa_ch32z1sB1w" }
-    - { seq: "B37Gv08hyS50De-CRjQGag", from: "Q7tbSDjFMaJHJ869L05WhQ", to: "-ppWuSBWTHHO0aYaqtXLFw" }
-    - { seq: "B37Gv08hyS50De-CRjQGag", from: "WmhxMtM7Ex4-Wx3xvR8ayQ", to: "l90xctfS3TZwX6b9nzbxJA" }
-    - { seq: "9G2U-cG8wwIGD4VpMfl5Og", from: "JS4EJ0O1A8Z1PerrGE2hmA", to: "CP20Gu7AnjK2JJBEGMzGBw" }
-    - { seq: "QhHtQR-RxBsqLC_eCwkIhw", from: "JqTQ6t3ouWDuKykE3KuZhg", to: "hRhiaoRnKk2Sf-NZmF3iGw" }
-    - { seq: "Y1cNn7zNI-PLOh7XHw8HCQ", from: "Qt9KtK_8nMicmX3uzSdy2A", to: "keR6u-Z00fxzW5A1FSWa3g" }
-    - { seq: "Oyyp1nAzddh3B4fppETpIA", from: "BUNvvyDpo0OFcP5Eos8cdQ", to: "zmaMXI7TVLxk0HPcX7FM9w" }
-    - { seq: "h7r09paz5mnxz5rs29tbft", from: "IRhNasrz7d7LP3EBbtVydY", to: "A4G2BjRNgJleVDJe33v4GQ" }
-    - { seq: "Oyyp1nAzddh3B4fppETpIA", from: "JaLVav70ZZTwH4PGaZMXJQ", to: "22T6eQDu7Ify_ZZSo0wiMA" }
-    - { seq: "9bgkZ_v6lXa0pFECmKaG4Q", from: "FtnhkvLf89jOWIapUTTWRA", to: "q7AvQ12-9YO7xGJF0lwsMg" }
-    - { seq: "bfW-O-H8tZ1v8Vnl8GKG9Q", from: "mcMCaQr-AWd70ydQ7HTBTg", to: "chL1_8_29IsEB1BgGo0ZAQ" }
-    - { seq: "WOUlpJCwAOOY-LhInIvOhA", from: "jR7VPCWiSWcQq3cLvxnGew", to: "iFJa1Tg1BUgezbuPkQnn3A" }
-    - { seq: "_4aCIgpsY2Fu_psoMnluMg", from: "6u43y-wczWgVHt-wDX1iHQ", to: "zC-BakOqmBe8LtJkbBK0LQ" }
-    - -YoxhyXB6MJixuKJ3isz0w
-    - { seq: "_4aCIgpsY2Fu_psoMnluMg", from: "zbgJ1DJT5d2RKnXxaJSRqQ", to: "mxOKAVXmXN3Nk0tSPI1Z8g" }
-    - { seq: "lxnFrt7ItSqoy65Rjb2zXg", from: "ck46BTd-9Gdt2FJL0Dimcw", to: "pMRuRQAHscvrHqop6bW-Aw" }
-    - { seq: "c1ogd5mbu6ujmdmbvyf7y4", from: "e-OXZp0_C-fzNFUt-6tSAA", to: "fqhSUMeBLpcA0F_WQEi-7Q" }
+  @behaviour Route.Behaviour
 
-    7 (stadteinwärts von Rahlstedt):
-    - { seq: "c1ogd5mbu6ujmdmbvyf7y4", from: "G3gPS6sKOOhPH__ZnLrZ5A", to: "oHqtO6MagdGz58_dtNL8Mw" }
-    - { seq: "fd3c4ib278az5de4c8l2eb", from: "_6kEAjXuYJ8Y-gFh8b8amg", to: "yUaXIGR69ohcD5T8D0ivOw" }
-    - { seq: "r9dAV4z8FWj9AdQg5OgwQw", from: "DSi2djCrg9DJmIk7G79iDQ", to: "WgDAbg-RZt8xO3ft1dWVbg" }
-    - { seq: "6WrroH_H6hOpHvTQ0WLRtg", from: "lOvJH09wQJgaHp6GOxU57Q", to: "t2MOJph-hOXBc4hHTuoQuA" }
-    - { seq: "C3QLu2CWyFLUW7u59LHhcg", from: "DU7fujTeYOq7fSZ3Ywgcyw", to: "XfFBFGy0oC0-GcmVxtPNDQ" }
-    - { seq: "C3QLu2CWyFLUW7u59LHhcg", from: "hSJz7qIFiqnmVbwGBiGidA", to: "clI6zrDiLG4h3MvIoBozMw" }
-    - { seq: "C3QLu2CWyFLUW7u59LHhcg", from: "ZkfB96Ucl-XN_f3yPMHY2Q", to: "D3OLbpe7ngAs00NdWMWAgw" }
-    - { seq: "dmy0FhsL-oe52Vm74TkHdA", from: "Ka2LXlPNlAGGT2Fq7pk14g", to: "MbBwhax6j1OEj-R2IDbGOA" }
-    - { seq: "7gfS5FO0LzXTRpBgxWWgHQ", from: "I4TlahZvYAk78egwhR3gGQ", to: "a1e9eKEQ_O6hB3kyHoeEnQ" }
-    - { seq: "7gfS5FO0LzXTRpBgxWWgHQ", from: "kSRGXSvuQaUa-PwzyCmUHA", to: "KJXyX3ua_W7xDc5CUNyEpg" }
-    - { seq: "aJyNnYP3XhAwhvGEKRTHag", from: "abkmG5qAe574kSdktPBupQ", to: "XP9sT826QYOrnj82P4nS_Q" }
-    - { seq: "h7r09paz5mnxz5rs29tbft", from: "MgHcAzOzqGI147LrPE5AvY", to: "b33zOiFZjHsIG8GL2Z2ODA" }
-    - { seq: "Edu4JcGlO-To_AK9zXhM0w", from: "0z-SUfDokmF7RrmhFkWgOA", to: "AQlRVmVRjQTMdE7n1hMhpQ" }
-    - { seq: "Edu4JcGlO-To_AK9zXhM0w", from: "ZfqVSnuF4A6NAH2atppsJQ", to: "teMpNHrHGruqTKgnOlrAjQ" }
-    - { seq: "CI5s5YQOVVLy9FQVDUnsOg", from: "9VMIH9mTpLpxzc-tv3FWhw", to: "BNAVug2jcb6GprFoKQ7zig" }
-    - { seq: "CI5s5YQOVVLy9FQVDUnsOg", from: "KpgnF6BqX-kaAtaUah6Nvw", to: "6GvQa2rOL7FKfBWZ_LA5cA" }
-    - { seq: "CI5s5YQOVVLy9FQVDUnsOg", from: "pVOthoqwX9z1GzknfYW9dg", to: "SyqIleMIdQjhq9cghz1ZnQ" }
-    - { seq: "jiFGVjrrZnOGR0Zlu8JZ2w", from: "qhHbX13hmKucTIOY5yV-kA", to: "WHBTcXnJsyM5k_Wtj5ZBZA" }
-    - { seq: "QSl3dQRNQU-HlZtMowf_Sg", from: "Y_kAEikm4LbHahLDdUszZQ", to: "4vhTYWBPlP5sd7lzhGi1Mw" }
+  def id(), do: "7"
+  def color(), do: "#97ba22"
+  def name(), do: "Alltagsroute 7"
+  def osm_relation_ref(), do: "https://www.openstreetmap.org/relation/228035"
 
-    7 (stadteinwärts von Jenfeld):
-    - { seq: "F_u_OJM5fYQ_UX8pbTd8RA", from: "pu17FEbl-3pMvgZjaF28Bg", to: "SGta2rrLRKe7jlbzkG0WBw" }
+  fw_rahlstedt = %Video.Track{
+    group: "7",
+    direction: :forward,
+    from: "Innenstadt",
+    to: "Rahlstedt",
+    parent_ref: __MODULE__,
+    text: "aus der Innenstadt nach Rahlstedt",
+    videos: [
+      {"2021-07-03-veloroute10/GX012051", :start, "0:00:03.770"},
+      {"2021-06-27-veloroute9/GX012023", "0:00:56.356", :end},
+      {"2021-04-25-freizeitroute4/GX011376", "0:00:10.377", :end},
+      {"2021-04-25-freizeitroute4/GX011377", :start, "0:00:06.473"},
+      {"2021-06-25-velo8/GX011969", "0:00:06.974", :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011902", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011903", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011904", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011905", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011906", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011907", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011908", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011909", :start, "0:00:40.207"},
+      {"2021-06-22-velo7-und-pauli-altona/GX011909", "0:00:54.922", "0:01:36.229"},
+      {"2021-06-22-velo7-und-pauli-altona/GX011913", "0:00:07.741", :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011914", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011915", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011916", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011917", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011918", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011920", :start, "0:00:57.057"}
+    ]
+  }
 
-    7 (stadtauswärts nach Jenfeld):
-    - { seq: "WOUlpJCwAOOY-LhInIvOhA", from: "yktNaEt-GeVA-bL_Q69fCA", to: "qZ17xJxozpZBtw-OBVtntA" }
-    - { seq: "n02L-ECaEp4__88BmILFCg", from: "pS0J8cEMQDqDkfijQHjWNA", to: "LEikbd5jcgTFchqs9-BFpg" }
-    - { seq: "F_u_OJM5fYQ_UX8pbTd8RA", from: "Yb_TQlveVTrYNofhwmkq5g", to: "Yj29r6x6unMCsu2f1_PPzg" }
-    """
+  bw_rahlstedt = %Video.Track{
+    group: "7",
+    direction: :backward,
+    from: "Rahlstedt",
+    to: "Innenstadt",
+    parent_ref: __MODULE__,
+    text: "aus Rahlstedt in die Innenstadt",
+    videos: [
+      {"2021-06-22-velo7-und-pauli-altona/GX011921", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011922", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011923", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011924", :start, "0:01:20.647"},
+      {"2021-06-22-velo7-und-pauli-altona/GX011925", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011926", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011927", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011928", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011930", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011931", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011932", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011933", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011934", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011935", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011937", :start, "0:00:47.180"}
+    ]
+  }
+
+  fw_jenfeld = %Video.Track{
+    group: "7j",
+    direction: :forward,
+    from: "Innenstadt",
+    to: "Jenfeld",
+    parent_ref: __MODULE__,
+    text: "aus der Innenstadt nach Jenfeld",
+    videos: [
+      {"2021-06-22-velo7-und-pauli-altona/GX011909", "0:01:37.097", :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011910", :start, "0:00:03.170"}
+    ]
+  }
+
+  bw_jenfeld = %Video.Track{
+    group: "7j",
+    direction: :backward,
+    from: "Jenfeld",
+    to: "Innenstadt",
+    parent_ref: __MODULE__,
+    text: "aus Jenfeld in die Innenstadt",
+    videos: [
+      {"2021-06-22-velo7-und-pauli-altona/GX011911", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011912", :start, :end},
+      {"2021-06-22-velo7-und-pauli-altona/GX011913", :start, "0:00:02.269"}
+    ]
+  }
+
+  @tracks [fw_rahlstedt, bw_rahlstedt, fw_jenfeld, bw_jenfeld]
+  def tracks(), do: @tracks
+
+  # legacy
+  def parsed() do
+    %{{id(), ""} => []}
+  end
+
+  def index() do
+    %{}
+  end
 end
