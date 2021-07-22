7ec302998e3a5704837a00548ebda9a29357c5f7
Stefan Breunig <stefan-github@yrden.de>
2021-07-23 01:40:08 +0200

    generate thumbs as webp if supported

--- lib/veloroute_web/controllers/image_extract_controller.ex
+++ lib/veloroute_web/controllers/image_extract_controller.ex
@@ -11,9 +11,10 @@ defmodule VelorouteWeb.ImageExtractController do
     with {ts_in_ms, ""} <- Integer.parse(ts),
          ren when not is_nil(ren) <- Video.Rendered.get(hash),
          true <- ren.length_ms() >= ts_in_ms,
-         {img, 0} <- ffmpeg(hash, ts_in_ms) do
+         webp <- supports_webp?(conn),
+         {img, 0} <- ffmpeg(hash, ts_in_ms, webp) do
       conn
-      |> put_resp_content_type("image/jpeg")
+      |> put_resp_content_type(if(webp, do: "image/webp", else: "image/jpeg"))
       |> put_resp_header("x-robots-tag", "noindex")
       |> put_resp_header("cache-control", "public, max-age=31536000, immutable")
       |> send_resp(200, img)
@@ -32,26 +33,38 @@ defmodule VelorouteWeb.ImageExtractController do
     |> halt
   end
 
-  defp ffmpeg(hash, ts) do
+  defp supports_webp?(conn) do
+    with ["" <> accept] <- get_req_header(conn, "accept") do
+      String.contains?(accept, "image/webp")
+    else
+      _ -> false
+    end
+  end
+
+  defp ffmpeg(hash, ts, webp) do
     ts = Video.Timestamp.from_milliseconds(ts)
     path = Path.join([Settings.video_target_dir_abs(), hash, @extract_from_file])
 
-    System.cmd("ffmpeg", [
-      "-hide_banner",
-      "-loglevel",
-      "error",
-      "-ss",
-      ts,
-      "-noaccurate_seek",
-      "-i",
-      path,
-      "-qscale:v",
-      "8",
-      "-frames:v",
-      "1",
-      "-f",
-      "mjpeg",
-      "-"
-    ])
+    System.cmd(
+      "ffmpeg",
+      [
+        "-hide_banner",
+        "-loglevel",
+        "error",
+        "-ss",
+        ts,
+        "-noaccurate_seek",
+        "-i",
+        path,
+        "-qscale:v",
+        if(webp, do: "20", else: "8"),
+        "-frames:v",
+        "1",
+        "-f",
+        if(webp, do: ["webp", "-preset", "photo"], else: "mjpeg"),
+        "-"
+      ]
+      |> List.flatten()
+    )
   end
 end
