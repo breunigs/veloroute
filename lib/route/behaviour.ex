e815793e80c96c6b28fe8eb9cc173d39ca0e99c5
Stefan Breunig <stefan-github@yrden.de>
2021-09-11 09:09:55 +0200

    MBA süd

--- lib/route/behaviour.ex
+++ lib/route/behaviour.ex
@@ -2,10 +2,10 @@ defmodule Route.Behaviour do
   @type color_hex() :: <<_::56>>
 
   @callback id() :: binary()
-  @callback type() :: :alltag | :freizeit
+  @callback type() :: :alltag | :freizeit | :rsw
   @callback color() :: color_hex()
   @callback name() :: binary()
   @callback article() :: binary()
-  @callback osm_relation_ref() :: binary()
+  @callback osm_relation_ref() :: binary() | nil
   @callback tracks() :: [Video.Track.t()]
 end
