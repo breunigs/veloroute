62c981e24c4a502382543b515e7c542bf7326376
Stefan Breunig <stefan-github@yrden.de>
2021-12-05 11:25:09 +0100

    speed up map compilation

--- lib/cache/map.ex
+++ lib/cache/map.ex
@@ -2,8 +2,8 @@ defmodule Cache.Map do
   @external_resource Map.Parser.default_map_path()
   @data Benchmark.measure("#{__MODULE__}: loading", &Map.Parser.load_default/0)
 
-  def nodes, do: @data.nodes
-  def ways, do: @data.ways
-  def relations, do: @data.relations
+  def nodes, do: full_map().nodes
+  def ways, do: full_map().ways
+  def relations, do: full_map().relations
   def full_map, do: @data
 end
