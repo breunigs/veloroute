bbc21e8af43c4718a141c620a510e1dbf02511e2
Stefan Breunig <stefan-github@yrden.de>
2022-01-08 20:41:27 +0100

    generate fallback videos from already converted ones

--- tools/video_convert_streamable.rb
+++ tools/video_convert_streamable.rb
@@ -2,13 +2,14 @@
 
 require 'tmpdir'
 require 'ruby-progressbar'
+require 'open3'
 
 VIDEO_OUT_DIR = ARGV[0]
 PARENT_DIR = File.dirname(VIDEO_OUT_DIR)
+NAME = File.basename($0, ".*")
 
 def die(msg)
-  warn "video_convert_streamable: #{msg}"
-  $stdin.close
+  warn "#{NAME}: #{msg}"
   exit(1)
 end
 
@@ -55,10 +56,10 @@ CODEC_HEVC = can_use?("hevc_nvenc") \
   ? %w[hevc_nvenc -preset slow -tier:v:__INDEX__ high -level:v:__INDEX__ 6.2 -nonref_p 1 -spatial_aq 1 -tag:v:__INDEX__ hvc1 -refs:v:__INDEX__ 0] \
   : %w[libx265 -x265-params log-level=error -tag:v:__INDEX__ hvc1]
 VARIANTS = [
-  {title: "360p",       width:  640, height:  360, bitrate:  4,   codec: CODEC_AVC},
+  {title: "360p",       width:  640, height:  360, bitrate:  4,   codec: CODEC_AVC, fallback: :mp4},
   {title: "144p",       width:  256, height:  144, bitrate:  0.7, codec: CODEC_AVC},
   {title: "240p",       width:  426, height:  240, bitrate:  2,   codec: CODEC_AVC},
-  {title: "480p",       width:  854, height:  480, bitrate:  4,   codec: CODEC_VP9, tag_as: "vp09.00.30.08"},
+  {title: "480p",       width:  854, height:  480, bitrate:  4,   codec: CODEC_VP9, tag_as: "vp09.00.30.08", fallback: :webm},
   {title: "720p",       width: 1280, height:  720, bitrate:  6,   codec: CODEC_AVC},
   {title: "1080p",      width: 1920, height: 1080, bitrate: 12,   codec: CODEC_AVC},
   {title: "1080p (HD)", width: 1920, height: 1080, bitrate: 12,   codec: CODEC_HEVC, tag_as: "hvc1.1.4.L186.B01"},
@@ -72,11 +73,15 @@ MAX_BITRATE = 1.1
 # https://trac.ffmpeg.org/wiki/Limiting%20the%20output%20bitrate
 BUF_SIZE = 2.0
 
-tmp_dir = Dir.mktmpdir("video_convert_streamable__#{File.basename(VIDEO_OUT_DIR)}__")
-at_exit { FileUtils.rmdir(tmp_dir) if Dir.exist?(tmp_dir) && Dir.empty?(tmp_dir) }
+TMP_DIR = Dir.mktmpdir("video_convert_streamable__#{File.basename(VIDEO_OUT_DIR)}__")
+at_exit { FileUtils.rmdir(TMP_DIR) if Dir.exist?(TMP_DIR) && Dir.empty?(TMP_DIR) }
+
+def nice
+  %w[nice -n18 ionice -c 3]
+end
 
 def ffmpeg
-  cmd = %w[nice -n18 ionice -c 3]
+  cmd = nice()
   cmd << %w[ffmpeg -hide_banner -loglevel fatal]
   cmd << %w[-hwaccel auto]
   cmd << %w[-re -f matroska -r] << FPS.to_s << %w[-i -] << "-r" << FPS.to_s
@@ -85,96 +90,94 @@ def ffmpeg
   cmd
 end
 
-# mp4 (fallback and ie11)
-fallback_mp4 = ffmpeg()
-fallback_mp4 << "-c:v" << VARIANTS[0][:codec]
-fallback_mp4 << "-s" << "#{VARIANTS[0][:width]}x#{VARIANTS[0][:height]}"
-rate = VARIANTS[0][:bitrate]
-fallback_mp4 << ["-b:v", "#{rate}M", "-maxrate", "#{rate * MAX_BITRATE}M", "-bufsize", "#{rate*BUF_SIZE}M"]
-fallback_mp4 << %w[-movflags +faststart]
-fallback_mp4 << "#{tmp_dir}/fallback.mp4"
+def variant_flags(idx)
+  var = VARIANTS[idx]
+  codec = var[:codec].flatten.map { |x| x.gsub("__INDEX__", "#{idx}") }
 
-# webm (no h264 installed?). We use a decent quality here, since presumably it's
-# some open source licencing issue, and not bandwidth/performance related.
-fallback_webm = ffmpeg()
-fallback_webm << %w[-c:v libvpx-vp9 -row-mt 1]
-fallback_webm << "-s" << "#{VARIANTS[3][:width]}x#{VARIANTS[3][:height]}"
-rate = VARIANTS[3][:bitrate]
-fallback_webm << ["-b:v", "#{rate}M", "-maxrate", "#{rate * MAX_BITRATE}M", "-bufsize", "#{rate*BUF_SIZE}M"]
-fallback_webm << "#{tmp_dir}/fallback.webm"
-# ffmpeg recommends two pass, but that's not straight forward to implement with
-# the current approach
-# fallback_webm_pass1 += ["-pass", "1", "-f", "null", "/dev/null"]
-# fallback_webm_pass2 += ["-pass", "2", "#{tmp_dir}/fallback.webm"]
-
-
-# hls
-hls = ffmpeg()
-VARIANTS.each.with_index do |var, idx|
-  codec = var[:codec].map { |x| x.gsub("__INDEX__", "#{idx}") }
-
-  hls << "-c:v:#{idx}" << codec << "-flags" << "+cgop"
-  hls << %w[-map v:0] << "-s:#{idx}" << "#{var[:width]}x#{var[:height]}"
-  hls << "-metadata:s:v:#{idx}" << %|title="#{var[:title]}"|
+  cmd = []
+  cmd << "-c:v:#{idx}" << codec << "-flags" << "+cgop"
+  cmd << %w[-map v:0] << "-s:#{idx}" << "#{var[:width]}x#{var[:height]}"
+  cmd << "-metadata:s:v:#{idx}" << %|title="#{var[:title]}"|
   rate = var[:bitrate]
-  hls << ["-b:v:#{idx}", "#{rate}M"]
-  hls << ["-maxrate:#{idx}", "#{rate * MAX_BITRATE}M"]
-  hls << ["-bufsize:#{idx}", "#{rate*BUF_SIZE}M"]
+  cmd << ["-b:v:#{idx}", "#{rate}M"]
+  cmd << ["-maxrate:#{idx}", "#{rate*MAX_BITRATE}M"]
+  cmd << ["-bufsize:#{idx}", "#{rate*BUF_SIZE}M"]
+
+  cmd
 end
-hls << %w[-movflags +faststart]
-hls << %w[-f hls -hls_time] << HLS_TIME << %w[-hls_playlist_type vod]
-hls << %w[-hls_segment_type fmp4]
-hls << %w[-master_pl_name] << "stream.m3u8"
-hls << %w[-hls_flags single_file+independent_segments -hls_list_size 0]
-hls << %w[-var_stream_map] << VARIANTS.map.with_index { |_, idx| "v:#{idx}"}.join(" ")
-hls << "#{tmp_dir}/stream_%v.m3u8"
 
-$ios = []
-$stdin.binmode
+def hls()
+  cmd = %w[-movflags +faststart]
+  cmd << %w[-f hls -hls_time] << HLS_TIME << %w[-hls_playlist_type vod]
+  cmd << %w[-hls_segment_type fmp4]
+  cmd << %w[-master_pl_name] << "stream.m3u8"
+  cmd << %w[-hls_flags single_file+independent_segments -hls_list_size 0]
+  cmd << %w[-var_stream_map] << VARIANTS.map.with_index { |_, idx| "v:#{idx}"}.join(" ")
+  cmd << "#{TMP_DIR}/stream_%v.m3u8"
+  cmd
+end
 
-def cancel
-  warn "\nvideo_convert_streamable: Aborting…"
+def fallback(type)
+  idx = VARIANTS.index { |v| v[:fallback] == type }
+  cmd = nice()
+  cmd << %w[ffmpeg -hide_banner -loglevel fatal]
+  cmd << ["-i", "#{TMP_DIR}/stream_#{idx}.m4s"]
+  cmd << %w[-c:v copy -movflags +faststart]
+  cmd << [File.join(TMP_DIR, "fallback.#{type}")]
+  cmd.flatten
+end
+
+def cancel(message = "Aborting…")
+  warn "\n#{NAME}: #{message}"
   $stdin.close unless $stdin.closed?
-  $ios.each { |io| Process.kill("KILL", io.pid) unless io.closed? }
-  $ios.each(&:close)
+  RUNNING_CHILDREN.each { |pid| Process.kill("KILL", pid) rescue nil }
   exit(2)
 end
+
+def run_pipe(cmd)
+  input, wait_threads = Open3.pipeline_w(cmd.flatten)
+
+  $stdin.binmode
+
+  wait_threads.each do |wt|
+    RUNNING_CHILDREN << wt.pid
+  end
+
+  while buf = $stdin.read(1024*1024) do
+    begin
+      input.write(buf)
+    rescue IOError
+      break
+    end
+  end
+  input.close
+  $stdin.close
+ensure
+  wait_threads.each do |wt|
+    exit_status = wt.value
+    RUNNING_CHILDREN.delete(wt.pid)
+    next if exit_status == 0
+    cancel(<<~ERROR)
+      subprocess failed ↑
+      CONVERT: #{cmd.flatten.join(" ")}
+    ERROR
+  end
+end
+
+RUNNING_CHILDREN = []
 
 Signal.trap("TERM") { cancel() }
 Signal.trap("INT") { cancel() }
 
-begin
-  $ios << IO.popen(fallback_mp4.flatten, "w+")
-  $ios << IO.popen(fallback_webm.flatten, "w+")
-  $ios << IO.popen(hls.flatten, "w+")
+puts "\n#{NAME}: hls…"
+run_pipe(ffmpeg() + VARIANTS.size.times.flat_map { |idx| variant_flags(idx) } + hls())
 
-  loop do
-    break if $stdin.closed?
-    buf = $stdin.readpartial(1024*1024)
-    $ios.map do |io|
-      Thread.new do
-        io.write(buf)
-      rescue IOError
-        # ignore error if we are in shutdown mode
-        raise unless $stdin.closed?
-      end
-    end.map(&:join)
-  rescue EOFError
-    # stdin is empty
-    $stdin.close
-    break
-  end
-end
-
-rendering_ok = $ios.map { |io| io.close(); $?.success? }.all?
-
-if !rendering_ok
-  cancel()
-  die("rendering failed!")
-end
+puts "\n#{NAME}: fallbacks…"
+system(*fallback(:mp4), exception: true)
+system(*fallback(:webm), exception: true)
 
 # manually tag codecs if ffmpeg didn't set them
-stream_path = File.join(tmp_dir, "stream.m3u8")
+stream_path = File.join(TMP_DIR, "stream.m3u8")
 manifest = File.read(stream_path)
 manifest.gsub!(/^#EXT-X-STREAM-INF:.*$/).with_index do |line, idx|
   tag = VARIANTS[idx][:tag_as]
@@ -187,11 +190,11 @@ File.write(stream_path, manifest)
 print "\nUploading… "
 tries = 0
 begin
-  FileUtils.move(tmp_dir, VIDEO_OUT_DIR, verbose: true)
+  FileUtils.move(TMP_DIR, VIDEO_OUT_DIR, verbose: true)
 rescue => e
   tries += 1
   warn "video_convert_streamable: Failed moving (try #{tries}): #{e}"
-  tries <= 3 ? retry : die("Failed moving the files from #{tmp_dir} to #{VIDEO_OUT_DIR}: #{e}")
+  tries <= 3 ? retry : die("Failed moving the files from #{TMP_DIR} to #{VIDEO_OUT_DIR}: #{e}")
 end
 
 puts "Done!"
