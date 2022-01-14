89f93c8e657ed8fe51b4b20eac5ef1df1a3ed29d
Stefan Breunig <stefan-github@yrden.de>
2022-01-14 21:05:41 +0100

    add graphic for RiMs

--- data/articles/static/lexikon-radfahrstreifen-in-mittellage.ex
+++ data/articles/static/lexikon-radfahrstreifen-in-mittellage.ex
@@ -11,7 +11,14 @@ defmodule Data.Article.Static.LexikonRadfahrstreifenInMittellage do
 
   def text(assigns) do
     ~H"""
-    <p>Umgangssprachlich als „Fahrradweiche“ bezeichnet ist ein „Radfahrstreifen in Mittellage“ eine an Kreuzungen aufgemalte Verkehrsführung. Sie packt die Geradeausspur für den Radverkehr zwischen die Geradeausspur bzw. Rechtsabbiegerspur für KFZ. Beispiel: <.m bounds="10.01356,53.61575,10.018941,53.617472" lon="10.0169467" lat="53.6161836" dir="forward" ref="4">Kreuzung Maienweg/Semmelmannstraße</.m>. Eine <.a href="https://nationaler-radverkehrsplan.de/de/aktuell/nachrichten/einsatzbereiche-von-radfahrstreifen-mittellage">Studie der TU Berlin</.a> kommt zum Ergebnis, das diese Führung vergleichbar mit älteren Varianten ist was Unfälle betrifft.</p>
+    <p><.m bounds="10.01356,53.61575,10.018941,53.617472" lon="10.0169467" lat="53.6161836" dir="forward" ref="4">Videobeispiel Kreuzung Maienweg/Semmelmannstraße</.m></p>
+
+    <p>Umgangssprachlich als „Fahrradweiche“ bezeichnet ist ein „Radfahrstreifen in Mittellage“ eine an Kreuzungen aufgemalte Verkehrsführung. Sie packt die Geradeausspur für den Radverkehr zwischen die Geradeausspur bzw. Rechtsabbiegerspur für KFZ.
+    </p>
+
+    <img src="/images/rim.svg" class="roaddiagram" />
+
+    <p>Eine <.a href="https://nationaler-radverkehrsplan.de/de/aktuell/nachrichten/einsatzbereiche-von-radfahrstreifen-mittellage">Studie der TU Berlin</.a> kommt zum Ergebnis, das diese Führung vergleichbar mit älteren Varianten ist was Unfälle betrifft. Die Stadt einigte sich 2020 mit dem <.a href="https://radentscheid-hamburg.de/">Radentscheid Hamburg</.a> darauf, keine neuen Radfahrstreifen in Mittellage mehr zu planen. Da zwischen Planung und Umbau teils Jahre vergehen, taucht diese Variante stellenweise trotzdem noch auf (Stand: 2022).</p>
     """
   end
 end
