b5daac2dfbfeda4454f5b963c4900e4baeefba68
Stefan Breunig <stefan-github@yrden.de>
2022-01-23 18:55:29 +0100

    diagram Fahrbahn vs Nebenflächen

--- data/articles/static/lexikon-fahrbahn-und-nebenflaechen.ex
+++ data/articles/static/lexikon-fahrbahn-und-nebenflaechen.ex
@@ -18,6 +18,9 @@ defmodule Data.Article.Static.LexikonFahrbahnUndNebenflaechen do
   def text(assigns) do
     ~H"""
     <p>„Straße“ meint immer alles, also salopp von Hauswand bis Hauswand. Die Fahrbahn bezeichnet das Stück zwischen den Bordsteinen, die heute meist geteert wird. Die KFZ-Fahrspuren sind immer Teil der Fahrbahn, auch Radfahrstreifen zählen zur Fahrbahn. Als Nebenflächen bezeichnet man alles vom Randstein bis zur Hauswand, also Fußwege oder Hochbordradwege. Auch die in Hamburg häufig anzutreffenden „Baum/Parkplatz“-Streifen zählen zu den Nebenflächen.</p>
+
+    <img src="/images/fahrbahn-und-nebenflaechen.svg" class="roaddiagram" alt="Eine Straßenansicht von oben (Vogelperspektive), die den Unterschied zwischen Nebenflächen und Fahrbahn verdeutlicht."/>
+
     """
   end
 end
