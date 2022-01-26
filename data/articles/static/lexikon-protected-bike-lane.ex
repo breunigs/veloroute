3fc37a17a6e1a30278ea409fcc64666c6e5b594f
Stefan Breunig <stefan-github@yrden.de>
2022-01-26 20:23:14 +0100

    PBL graphic

--- data/articles/static/lexikon-protected-bike-lane.ex
+++ data/articles/static/lexikon-protected-bike-lane.ex
@@ -17,7 +17,13 @@ defmodule Data.Article.Static.LexikonProtectedBikeLane do
 
   def text(assigns) do
     ~H"""
-    <p>Unter einer „<.m lat="53.45947" lon="9.99094" dir="backward" ref="11" bounds="9.986734,53.458457,9.997266,53.465207">Protected Bike Lane</.m>“, also einer geschützten Radspur, versteht man eine vom Autoverkehr abgetrennte Fahrradspur. Eine Trennung wird durch einen extra Bordstein, Poller oder ähnliche Absperrelementen erreicht. Ihr Hauptvorteil liegt im besseren Fahrgefühl, da die Chancen sinkt das Autofahrende die Spur illegal mitbenutzen oder darauf parken.</p>
+    <p><.m lat="53.45947" lon="9.99094" dir="backward" ref="11" bounds="9.986734,53.458457,9.997266,53.465207">Videobeispiel einer Protected Bike Lane auf Veloroute 11</.m></p>
+
+
+    <p>Unter einer „Protected Bike Lane“, also einer geschützten Radspur, versteht man eine vom Autoverkehr abgetrennte Fahrradspur. Eine Trennung wird durch einen extra Bordstein, Poller oder ähnliche Absperrelementen erreicht. Ihr Hauptvorteil liegt im besseren Fahrgefühl, da die Chancen sinkt das Autofahrende die Spur illegal mitbenutzen oder darauf parken. In Hamburg sind häufig noch kleine, orangefarbene Poller darauf angebracht, um die Sichtbarkeit zu erhöhen.</p>
+
+    <.roaddiagram src="pbl" alt="Eine Straßenansicht von oben (Vogelperspektive) die ein Beispiel einer Protected-Bike-Lane auf beiden Straßenseiten zeigt."/>
+
     <p>Nicht verwechseln: „<.ref>Schutzstreifen</.ref>“ klingt sehr ähnlich, stellt den Radverkehr aber deutlich schlechter.</p>
     """
   end
