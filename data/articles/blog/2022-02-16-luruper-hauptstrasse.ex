7dfb3e599d9b0387bd9f89a0f5f1d2d5f2cb92ef
Stefan Breunig <stefan-github@yrden.de>
2022-02-16 18:09:11 +0100

    luruper hauptstraße

---
+++ data/articles/blog/2022-02-16-luruper-hauptstrasse.ex
@@ -0,0 +1,44 @@
+defmodule Data.Article.Blog.LuruperHauptstrasse do
+  use Article.Default
+
+  def created_at(), do: ~D[2022-02-16]
+
+  def title(), do: "Luruper Hauptstraße"
+  def summary(), do: "Stadt bittet um Beteiligung zur Gestaltung der Luruper Hauptstraße"
+
+  def type(), do: :intent
+  def tags(), do: []
+
+  def tracks(),
+    do: [
+      %Video.Track{
+        group: "Luruper Hauptstraße",
+        direction: :forward,
+        from: "",
+        to: "",
+        parent_ref: __MODULE__,
+        text: "Luruper Hauptstraße Richtung Schenefeld",
+        videos: [
+          {"2022-01-16-wedel/GX013142", :start, :end},
+          {"2022-01-16-wedel/GX013143", :start, "00:01:04.798"}
+        ]
+      }
+    ]
+
+  def links(_assigns) do
+    [
+      {"Online-Beteiligung auf Seite der Stadt",
+       "https://mobilwandel2035.beteiligung.hamburg/dipas/#/"}
+    ]
+  end
+
+  def text(assigns) do
+    ~H"""
+    <p>Die Stadt bittet um Beteiligung zur zukünftigen Gestaltung der Luruper Hauptstraße. Das Projekt firmiert unter dem Titel „Mobilwandel 2035“ und wird vom Bundesministerium für Umwelt, Naturschutz und nukleare Sicherheit mitgetragen. Ziel sei die Luruper Hauptstraße fit für den Klimawandel zu machen und ihre Aufenthaltsqualität zu erhöhen. Mögliche Lösungen könnten dann auch auf ähnliche Straßen übertragen werden.</p>
+
+    <p>Die Stadt hat dazu eine Seite eingerichtet, auf der schriftliche Beiträge bis 31.03.2022 eingereicht werden können:</p>
+
+    <.structured_links/>
+    """
+  end
+end
