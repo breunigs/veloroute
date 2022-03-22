b0e5c1d4da6d3bca05dffa5b2b44f25fae189237
Stefan Breunig <stefan-github@yrden.de>
2022-03-22 19:51:37 +0100

    use the same frame rate as other videos

--- tools/video_deshake_time_lapse.rb
+++ tools/video_deshake_time_lapse.rb
@@ -5,7 +5,7 @@ require 'pry'
 # Convert a regular video to a time lapsed one.
 
 # The ffmpeg commands assume that the source video is a regular 23.976 FPS video.
-# Since time lapsed videos are usually at 29.97 FPS and 5x faster than a regular
+# Since time lapsed videos are usually at 30000/1001 FPS and 5x faster than a regular
 # video, the PTS is additionally adjusted to match the same speed.
 
 ARGV.each do |file|
@@ -27,11 +27,11 @@ ARGV.each do |file|
 
   Dir.mktmpdir("video_deshake_time_lapse") do |temp_dir|
     warn "  shake detection"
-    `nice -n18 ffmpeg -hide_banner -loglevel warning -r 29.97 -i "#{file}" -vf "vidstabdetect=stepsize=6:shakiness=8:accuracy=9:result=#{temp_dir}/shake.trf" -f null -`
+    `nice -n18 ffmpeg -hide_banner -loglevel warning -r 30000/1001 -i "#{file}" -vf "vidstabdetect=stepsize=6:shakiness=8:accuracy=9:result=#{temp_dir}/shake.trf" -f null -`
     binding.pry unless $?.success?
 
     warn "  fixing speed and smoothing"
-    `nice -n18 ffmpeg -hide_banner -loglevel warning -hwaccel auto -r 29.97 -i "#{file}" -vf "vidstabtransform=zoom=1:smoothing=30:input=#{temp_dir}/shake.trf,setpts=1/4*PTS" -an -c:v ffv1 "#{temp_dir}/time_lapse.mkv"`
+    `nice -n18 ffmpeg -hide_banner -loglevel warning -hwaccel auto -r 30000/1001 -i "#{file}" -vf "vidstabtransform=zoom=1:smoothing=30:input=#{temp_dir}/shake.trf,setpts=1/4*PTS" -an -c:v ffv1 "#{temp_dir}/time_lapse.mkv"`
     binding.pry unless $?.success?
 
     warn "  moving file to destination"
