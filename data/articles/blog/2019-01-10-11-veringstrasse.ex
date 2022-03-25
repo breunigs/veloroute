81689e5ec6eedfac9d21571114d7a8c6a60eb641
Stefan Breunig <stefan-github@yrden.de>
2022-03-25 18:41:23 +0100

    updates

--- data/articles/blog/2019-01-10-11-veringstrasse.ex
+++ data/articles/blog/2019-01-10-11-veringstrasse.ex
@@ -6,10 +6,11 @@ defmodule Data.Article.Blog.Veringstrasse do
   def updated_at(), do: ~D[2020-06-13]
   def title(), do: "Veringstraße (Veloroute 11)"
 
-  def start(), do: ~d[2022-03]
-  def stop(), do: ~d[2023-03]
+  def start(), do: ~d[2022-04-04]
+  def stop(), do: ~d[2023-03-05]
+  def construction_site_id_hh(), do: [33169]
 
-  def type(), do: :planned
+  def type(), do: :construction
 
   def tags(), do: ["11"]
 
