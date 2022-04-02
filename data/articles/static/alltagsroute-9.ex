465b4589c233cbec3acc2508ae74234dae99717f
Stefan Breunig <stefan-github@yrden.de>
2022-04-02 17:43:28 +0200

    velo9 wip

--- data/articles/static/alltagsroute-9.ex
+++ data/articles/static/alltagsroute-9.ex
@@ -15,7 +15,6 @@ defmodule Data.Article.Static.Alltagsroute9 do
   def tracks(),
     do: [
       %Video.Track{
-        fade: :none,
         group: "9 Bergedorf",
         direction: :forward,
         from: "Innenstadt",
@@ -23,24 +22,23 @@ defmodule Data.Article.Static.Alltagsroute9 do
         parent_ref: __MODULE__,
         text: "aus der Innenstadt nach Bergedorf",
         videos: [
-          {"2021-07-03-veloroute10/GX012051", :start, "00:00:03.770"},
-          {"2021-06-27-veloroute9/GX012023", "00:00:56.356", :end},
-          {"2021-04-25-freizeitroute4/GX011376", "00:00:10.377", :end},
-          {"2021-06-27-veloroute9/GX012024", "00:00:31.131", :end},
-          {"2021-06-27-veloroute9/GX012025", :start, :end},
-          {"2021-06-27-veloroute9/GX012026", :start, :end},
-          {"2021-06-27-veloroute9/GX012027", :start, "00:01:30.490"},
-          {"2021-09-19-rsw-geesthacht-velo9/GX012883", "00:00:15.415", "00:00:58.959"},
-          {"2021-06-27-veloroute9/GX012027", "00:02:23.510", :end},
-          {"2021-06-27-veloroute9/GX022027", :start, :end},
-          {"2021-06-27-veloroute9/GX012028", :start, :end},
-          {"2021-06-27-veloroute9/GX012029", :start, :end},
-          {"2021-06-27-veloroute9/GX012030", :start, :end},
-          {"2021-06-27-veloroute9/GX012031", :start, "00:00:12.312"}
+          {"2022-04-02-velo9/GX013399", "00:00:18.185", :end},
+          {"2022-04-02-velo9/GX013400", :start, :end},
+          {"2022-04-02-velo9/GX013401", :start, :end},
+          {"2022-04-02-velo9/GX013402", :start, :end},
+          {"2022-04-02-velo9/GX013403", :start, :end},
+          {"2022-04-02-velo9/GX013404", :start, :end},
+          {"2022-04-02-velo9/GX013405", :start, :end},
+          {"2022-04-02-velo9/GX013406", :start, :end},
+          {"2022-04-02-velo9/GX013407", :start, :end},
+          {"2022-04-02-velo9/GX013408", :start, :end},
+          {"2022-04-02-velo9/GX013409", :start, :end},
+          {"2022-04-02-velo9/GX013410", :start, :end},
+          {"2022-04-02-velo9/GX013411", :start, :end},
+          {"2022-04-02-velo9/GX013412", :start, "00:00:13.847"}
         ]
       },
       %Video.Track{
-        fade: :none,
         group: "9 Bergedorf",
         direction: :backward,
         from: "Bergedorf",
@@ -48,21 +46,20 @@ defmodule Data.Article.Static.Alltagsroute9 do
         parent_ref: __MODULE__,
         text: "aus Bergedorf in die Innenstadt",
         videos: [
-          {"2021-06-27-veloroute9/GX012031", "00:00:21.655", :end},
-          {"2021-06-27-veloroute9/GX012032", :start, :end},
-          {"2021-06-27-veloroute9/GX012033", :start, :end},
-          {"2021-06-27-veloroute9/GX012034", :start, :end},
-          {"2021-06-27-veloroute9/GX012035", :start, :end},
-          {"2021-06-27-veloroute9/GX022035", :start, :end},
-          {"2021-06-27-veloroute9/GX012036", :start, :end},
-          {"2021-06-27-veloroute9/GX012037", :start, "00:01:06.767"},
-          {"2021-09-19-rsw-geesthacht-velo9/GX012918", "00:01:06.066", "00:01:29.356"},
-          {"2021-06-27-veloroute9/GX012037", "00:01:46.540", :end},
-          {"2021-06-27-veloroute9/GX012038", :start, :end},
-          {"2021-06-27-veloroute9/GX012039", :start, :end},
-          {"2021-06-27-veloroute9/GX012040", :start, :end},
-          {"2021-06-27-veloroute9/GX012041", :start, "00:00:16.350"},
-          {"2021-06-22-velo7-und-pauli-altona/GX011937", "00:00:23.023", "00:00:47.180"}
+          {"2022-04-02-velo9/GX013412", "00:00:26.226", :end},
+          {"2022-04-02-velo9/GX013413", :start, :end},
+          {"2022-04-02-velo9/GX013414", :start, :end},
+          {"2022-04-02-velo9/GX013415", :start, :end},
+          {"2022-04-02-velo9/GX013416", :start, :end},
+          {"2022-04-02-velo9/GX023416", :start, "00:00:22.589"},
+          {"2022-04-02-velo9/GX023416", "00:00:50.617", :end},
+          {"2022-04-02-velo9/GX013417", :start, :end},
+          {"2022-04-02-velo9/GX013418", :start, :end},
+          {"2022-04-02-velo9/GX013419", :start, :end},
+          {"2022-04-02-velo9/GX013420", :start, :end},
+          {"2022-04-02-velo9/GX013421", :start, :end},
+          {"2022-04-02-velo9/GX013422", :start, :end},
+          {"2022-04-02-velo9/GX013424", :start, "00:00:21.588"}
         ]
       }
     ]
