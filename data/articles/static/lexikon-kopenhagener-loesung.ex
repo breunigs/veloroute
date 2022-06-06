606e2984b19553bf4aa3bb4dc3b2c60ed066bcce
Stefan Breunig <stefan-github@yrden.de>
2022-06-06 12:43:09 +0200

    make video option for Kopenhagener Lösung more obvious

--- data/articles/static/lexikon-kopenhagener-loesung.ex
+++ data/articles/static/lexikon-kopenhagener-loesung.ex
@@ -17,7 +17,9 @@ defmodule Data.Article.Static.LexikonKopenagenerLoesung do
 
   def text(assigns) do
     ~H"""
-    <p>Als <.m bounds="9.973614,53.536534,10.019039,53.546934" lon="10.0054612" lat="53.5408617" dir="backward" ref="10">Kopenhagener Lösung</.m> beschreibt man Radwege, die durch einen Bordstein von den KFZ-Spuren und dem Fußweg abgetrennt sind. Sie stellen damit eine Zwischenstufe von <.ref>Hochbordradwegen</.ref> und <.ref>Radfahrstreifen</.ref> dar.</p>
+    <p><.m bounds="9.999916,53.539131,10.010449,53.542032" lon="10.00548" lat="53.540836" dir="backward" ref="10">Videobeispiel Überseeallee in der HafenCity</.m></p>
+
+    <p>Als Kopenhagener Lösung beschreibt man Radwege, die durch einen Bordstein von den KFZ-Spuren und dem Fußweg abgetrennt sind. Sie stellen damit eine Zwischenstufe von <.ref>Hochbordradwegen</.ref> und <.ref>Radfahrstreifen</.ref> dar.</p>
     """
   end
 end
