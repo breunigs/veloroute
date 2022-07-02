c7eeaa1103c108085e989c6138fbad100ee71d7f
Stefan Breunig <stefan-github@yrden.de>
2022-07-02 13:51:35 +0200

    modernize plausible.js

---
+++ assets/js/plausible.ts
@@ -0,0 +1,60 @@
+export { };
+
+declare global {
+  interface Window {
+    plausible: (eventName: string, options?: { meta?: any, props?: any }) => void
+  }
+}
+
+type payload =
+  {
+    n: string,
+    u: string,
+    d: string,
+    r: string | null,
+    w: number,
+    m?: string,
+    p?: string,
+  }
+
+
+window.plausible = (eventName, options) => {
+  let payload: payload = {
+    n: eventName,
+    u: window.location.href,
+    d: window.location.hostname,
+    r: window.document.referrer || null,
+    w: window.innerWidth,
+  }
+  if (options?.meta) payload.m = JSON.stringify(options.meta)
+  if (options?.props) payload.p = JSON.stringify(options.props)
+
+  fetch('/plausible/event', {
+    method: 'POST',
+    headers: { 'Content-Type': 'text/plain' },
+    body: JSON.stringify(payload),
+    // @ts-ignore experimental feature, cmp. https://web.dev/priority-hints/#the-fetchpriority-attribute
+    priority: 'low',
+  })
+}
+
+let lastPage: string
+
+function page() {
+  if (lastPage === window.location.pathname) return;
+  lastPage = window.location.pathname
+  window.plausible('pageview')
+}
+
+if (window.history.pushState) {
+  let originalPushState = window.history.pushState
+  window.history.pushState = function () {
+    originalPushState.apply(this, arguments)
+    page()
+  }
+  window.addEventListener('popstate', page)
+}
+
+page()
+
+window.addEventListener('beforeprint', () => window.plausible('print'))
