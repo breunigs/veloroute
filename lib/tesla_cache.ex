01c433d2b4a4922d668da6765b97d4ef2313f6b3
Stefan Breunig <stefan-github@yrden.de>
2022-07-02 13:13:33 +0200

    hide latency for cache busted resources

--- lib/tesla_cache.ex
+++ lib/tesla_cache.ex
@@ -3,16 +3,38 @@ defmodule TeslaCache do
   @ttl :timer.hours(24)
 
   def call(env, next, _opts) do
-    if cache_busters?(env) do
-      Tesla.run(env, next)
-    else
-      run = fn ->
-        resp = Tesla.run(env, next)
-        {keep?(resp), resp}
-      end
+    {:ok, resp} = Cachex.get(:tesla_cache_cachex, cache_key(env))
 
-      {_status, resp} = Cachex.fetch(:tesla_cache_cachex, cache_key(env), run, ttl: @ttl)
-      resp
+    cond do
+      is_nil(resp) ->
+        fetch(env, next)
+
+      cache_busters?(env) ->
+        background_update(env, next)
+        resp
+
+      true ->
+        resp
+    end
+  end
+
+  defp fetch(env, next) do
+    {_status, resp} = Cachex.fetch(:tesla_cache_cachex, cache_key(env), run(env, next), ttl: @ttl)
+    resp
+  end
+
+  defp background_update(env, next) do
+    Task.start(fn ->
+      with {:commit, resp} <- run(env, next).() do
+        Cachex.put(:tesla_cache_cachex, cache_key(env), resp, ttl: @ttl)
+      end
+    end)
+  end
+
+  defp run(env, next) do
+    fn ->
+      resp = Tesla.run(env, next)
+      {keep?(resp), resp}
     end
   end
 
@@ -21,5 +43,8 @@ defmodule TeslaCache do
 
   defp cache_busters?(%Tesla.Env{query: query}), do: Enum.any?(query, &(elem(&1, 0) == "sku"))
 
-  defp cache_key(%Tesla.Env{url: url, query: query}), do: Tesla.build_url(url, query)
+  defp cache_key(%Tesla.Env{url: url, query: query}) do
+    query = Enum.reject(query, &(elem(&1, 0) == "sku"))
+    Tesla.build_url(url, query)
+  end
 end
