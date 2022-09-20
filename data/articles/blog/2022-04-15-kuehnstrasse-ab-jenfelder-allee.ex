2e4d0b93ad13728ef7f867275cb181d8ffc1e9ef
Stefan Breunig <stefan-github@yrden.de>
2022-09-20 18:27:29 +0200

    updates

--- data/articles/blog/2022-04-15-kuehnstrasse-ab-jenfelder-allee.ex
+++ data/articles/blog/2022-04-15-kuehnstrasse-ab-jenfelder-allee.ex
@@ -2,11 +2,13 @@ defmodule Data.Article.Blog.KuehnstrasseAbJenfelderAllee do
   use Article.Default
 
   def created_at(), do: ~D[2022-04-15]
+  def updated_at(), do: ~D[2022-09-20]
 
   def title(), do: "Kuehnstraße ab Jenfelder Allee (Alltagsroute 5)"
 
   def summary(),
-    do: "Anvisierter Bauzeitraum: 2024 bis 2025. Noch keine Pläne bekannt."
+    do:
+      "1,85m breite Radfahrstreifen. Umbau nur möglich, wenn die Kreuzung mit der Jenfelder Allee ebenfalls mit angepasst wird."
 
   def type(), do: :intent
   def start(), do: ~d[2024]
@@ -16,7 +18,11 @@ defmodule Data.Article.Blog.KuehnstrasseAbJenfelderAllee do
 
   def links(_assigns) do
     [
-      {"Möglicher Bautermin", "Stand 2022",
+      {"Problematik Kreuzung Jenfelder Allee", "September 2022",
+       "https://sitzungsdienst-wandsbek.hamburg.de/bi/vo020.asp?VOLFDNR=1017324"},
+      {"Pläne und Erläuterungen", "September 2022",
+       "https://sitzungsdienst-wandsbek.hamburg.de/bi/vo020.asp?VOLFDNR=1017326"},
+      {"Möglicher Bautermin", "April 2022",
        "https://sitzungsdienst-wandsbek.hamburg.de/bi/vo020.asp?VOLFDNR=1016643"}
     ]
   end
@@ -26,8 +32,16 @@ defmodule Data.Article.Blog.KuehnstrasseAbJenfelderAllee do
     <h4>Alter Zustand</h4>
     <p>In diesem Abschnitt der Kuehnstraße gibt es keine eigenen Radwege. Der Radverkehr teilt sich im <.ref>Mischverkehr</.ref> die Fahrbahn mit den KFZ. An der Kreuzung mit der <.m bounds="10.125769,53.581522,10.12832,53.583135" lon="10.127042" lat="53.582407" dir="forward" ref="7">Jenfelder Allee</.m> gibt es noch alte <.ref>Hochbordradwege</.ref>, die je nach Richtung schwer zu erreichen bzw. zu verlassen sind. Die Kreuzung mit der <.m bounds="10.131371,53.583898,10.135139,53.585823" lon="10.132449" lat="53.584436" dir="forward" ref="7">Wilsonstraße</.m> wurde vor einigen Jahren umgebaut und bietet Radwegstummel zum <.ref>indirekten Linksabbiegen</.ref>.</p>
 
-    <h4>Vorhaben</h4>
-    <p>Der Abschnitt wurde in die Baustellenplanung aufgenommen. Pläne sind noch nicht bekannt.</p>
+    <h4>Planung</h4>
+    <p>Es ist vorgesehen beidseitig rund 1,85m breite <.ref>Radfahrstreifen</.ref> einzurichten. Die Einmündung <.m bounds="10.129781,53.583744,10.132448,53.584673" lon="10.131055" lat="53.584248" dir="backward" ref="7">Wöschenhof</.m> soll zu einer Gehwegüberfahrt umgebaut werden. Die Gehwege werden gepflastert. Auf der Südseite sollen dazu Grundstücke gekauft werden, um die Mindestbreiten zu erreichen. Bei Fahrradanlehnbügeln und Stellplätzen für Lastenräder setzt man niedrige Bordsteine ein.</p>
+
+    <h5>Anschluss Jenfelder Allee</h5>
+    <p>Aus der Innenstadt kommend wird der schmale <.ref>Hochbordradweg</.ref> als Übergangslösung auf den Radfahrstreifen abgeleitet. Die Fahrbahn verengt sich hier von zwei auf eine Fahrspur. In der Gegenrichtung endet der Radfahrstreifen in der kombinierten Geradeaus- und Rechtsabbiegerspur.</p>
+
+    <p>Dies wird als nicht verkehrssicher angesehen, sodass die Kreuzung mit der <.m bounds="10.126022,53.581484,10.127903,53.583278" lon="10.126946" lat="53.58255" dir="backward" ref="7">Jenfelder Allee</.m> ebenfalls mit umgebaut werden muss. Ursprünglich war vorgesehen die gesamte Jenfelder Allee zu erneuern, was aber auf unbestimmte Zeit verschoben wurde. Stattdessen soll die Kreuzung getrennt geplant und umgebaut werden – nur wenn das gelingt, kann der beschriebene Umbau der Kuehnstraße erfolgen.</p>
+
+    <h4>Meinung</h4>
+    <p>Man führt die Buslinien und das hohe KFZ Aufkommen an, um nicht am Status Quo rütteln zu müssen. Trotz Grundstückzukauf und Baumfällungen auf der Südseite bleiben dem Radverkehr nur unzeitgemäße Breiten, noch dazu ohne Trennung vom KFZ Verkehr. Mindestens die Bauweise „<.ref>Kopenhagener Radweg</.ref>“ sollte geprüft werden. Eine bessere Lösung erfordert weiträumige Planungen um die KFZ von der Veloroute wegzulenken. Besser als vorher ist es aber allemal.</p>
 
     <h4>Quelle</h4>
     <.structured_links/>
