42c83d2ea7cf78bf427f321b4fe9e031d87e9aa1
Stefan Breunig <stefan-github@yrden.de>
2022-12-06 18:44:47 +0100

    use more standard approach to dynamically load config

--- config/prod.exs
+++ config/prod.exs
@@ -1,11 +1,8 @@
 import Config
-import_config "../data/settings.ex"
 
-parsed = URI.new!(Settings.url())
 port = String.to_integer(System.get_env("PORT") || "4000")
 
 config :veloroute, VelorouteWeb.Endpoint,
-  url: [host: parsed.host, port: parsed.port, scheme: parsed.scheme],
   cache_static_manifest: "priv/static/cache_manifest.json",
   http: [
     port: port,
@@ -16,7 +13,8 @@ config :veloroute, VelorouteWeb.Endpoint,
     rewrite_on: [:x_forwarded_proto, :x_forwarded_host, :x_forwarded_port],
     # added by nginx
     hsts: false
-  ]
+  ],
+  use_url_from_settings: true
 
 # Do not print debug messages in production
 config :logger, level: :info
