5911d941e9902d6cb6bd62f07d385a60b34ab3ec
Stefan Breunig <stefan-github@yrden.de>
2023-01-17 21:33:08 +0100

    updates

--- data/articles/blog/2023-01-17-garstedter-weg.ex
+++ data/articles/blog/2023-01-17-garstedter-weg.ex
@@ -26,25 +26,25 @@ defmodule Data.Article.Blog.GarstedterWeg do
   def text(assigns) do
     ~H"""
     <h4>Alter Zustand</h4>
-    <p>Stellenweise sind schmale <.ref>Hochbordradwege</.ref> vorhanden, die jedoch unvermittelt aufhören und zu reinen Gehwegen werden. Aus dem <.m bounds="9.971391,53.642904,9.976052,53.64581" lon={9.972833} lat={53.643581} dir="backward" ref="14">Moorrand</.m> ist das Linksabbiegen häufig schwierig. In der Gegenrichtung ist die Weiterfahrt vom Zweirichtungsradweg völlig unklar und benötigt Ortskenntnisse.</p>
+    <p>Stellenweise sind schmale <.ref>Hochbordradwege</.ref> vorhanden, die jedoch unvermittelt aufhören und zu reinen Gehwegen werden. Aus dem <.m bounds="9.971391,53.642904,9.976052,53.64581" lon={9.972351} lat={53.643417} dir="forward" ref="14">Moorrand</.m> ist das Linksabbiegen häufig schwierig. In der Gegenrichtung ist die Weiterfahrt <.m bounds="9.971391,53.642904,9.976052,53.64581" lon={9.975374} lat={53.645562} dir="backward" ref="14">vom Zweirichtungsradweg</.m> unklar und benötigt Ortskenntnisse.</p>
 
     <h4>Planung</h4>
-    <p>Die Einmündung des Moorrands wird zu einem Kreisverkehr umgebaut.</p>
+    <p>Die Einmündung Moorrand wird zu einem Kreisverkehr umgebaut.</p>
 
     <p>In Richtung Flughafen folgt ein Stück <.ref>Mischverkehr</.ref>, dann <.ref>Kopenhagener Radweg</.ref>, dann <.ref>Radfahrstreifen</.ref> um die Einmündung <.m bounds="9.971391,53.642904,9.976052,53.64581" lon={9.97395} lat={53.644436} dir="forward" ref="14">Vierenkamp</.m>, und schließlich ein Hochbordradweg – bis auf den Abschnitt im Mischverkehr alle rund 2,60m breit.</p>
 
     <p>In der Gegenrichtung soll weiterhin am <.m bounds="9.971391,53.642904,9.976052,53.64581" lon={9.974647} lat={53.645383} dir="backward" ref="14">Swebenweg</.m> die Straßenseite gewechselt werden, die Kreuzung wird nicht umgebaut. Zunächst fährt man über die Wartefläche des Busses, dann ein Stück <.ref>Schutzstreifen</.ref> (2,0m breit), Kopenhagener Radweg (2,60m), Radfahrstreifen um die Einmündung <.m bounds="9.971391,53.642904,9.976052,53.64581" lon={9.973854} lat={53.64472} dir="backward" ref="14">Vierenkamp</.m> (2,60m), Kopenhagener Radweg (zunächst 2,60m, verengt sich auf 1,60m) und schließlich ein Schutzstreifen der in den Mischverkehr überleitet. Die Gestaltung ist dabei so, dass die Radspur immer schmaler wird, die KFZ-Spur hingegen durchgehend gleich breit bleibt.</p>
 
-    <p>Außerhalb der Veloroute wird die <.m bounds="9.97162,53.642338,9.973019,53.643597">Bushhaltestelle in Richtung Flughafen</.m> vor den Kreisverkehr gelegt. An ihr führt ein 2,5m breiter Radfahrstreifen vorbei. Die Gestaltung ist dabei so, dass die Radspur gleich breit bleibt und die KFZ-Spur immer schmaler wird, bis es in den Mischverkehr für den Kreisel übergeht.</p>
+    <p>Außerhalb der Veloroute wird die <.m bounds="9.97162,53.642338,9.973019,53.643597" lon={9.972921} lat={53.643491} dir="forward" ref="14">Bushaltestelle in Richtung Flughafen</.m> vor den Kreisverkehr gelegt. An ihr führt ein 2,5m breiter Radfahrstreifen vorbei. Die Gestaltung ist dabei so, dass die Radspur gleich breit bleibt und die KFZ-Spur immer schmaler wird, bis es in den Mischverkehr für den Kreisel übergeht.</p>
 
     <p>Es kommen einige Radanlehnbügel und Sitzbänke hinzu.</p>
 
     <h4>Meinung</h4>
     <p>Die Planung ist mangelhaft:</p>
     <ul>
-      <li>Der Radverkehr in Richtung Moorreihe wird regelrecht an die Seite gedrängt. Es ist realitätsfern anzunehmen, dass Reißverschlussverfahren oder Überholabstände eingehalten werden, sodass hier eine neue Gefahrenstelle geplant wurde.</li>
+      <li>Der Radverkehr in Richtung Moorreihe wird regelrecht an die Seite gedrängt. Es ist realitätsfern anzunehmen, dass Reißverschlussverfahren oder Überholabstände eingehalten werden, sodass hier eine neue Gefahrenstelle geschaffen wird.</li>
       <li>Die ständigen Auf- und Ableitungen machen den Radweg zu einer Huckelpiste. Außerdem setzt man weiterhin auf Pflastersteine statt besser befahrbarem Asphalt.</li>
-      <li>Die Überleitung vom Zweirichtungsradweg fehlt weiterhin. Da der Hochbordradweg breiter wird, ist es sogar noch unklarer, dass die Straßenseite gewechselt werden soll. Wer dennoch weiterfährt strandet dann am Vierenkamp, weil Radverkehr in diese Richtung nicht mehr vorgesehen ist.</li>
+      <li>Die fehlende Überleitung vom Zweirichtungsradweg wird schlimmer, da der Hochbord breiter wird als bisher. Es ist daher nicht ersichtlich, dass am Swebenweg gequert werden soll und nicht erst am Vierenkamp. Dort darf man dann auch nicht mehr legal die Ampel queren, was heute noch erlaubt ist.</li>
     </ul>
 
     <p>Diese eklatanten Probleme kann auch der gelungene Kreisverkehr nicht mehr wettmachen. Es ist zu hoffen, dass dieser erste Entwurf stark überarbeitet wird und eine sichere und komfortable Lösung für den Radverkehr geschaffen wird.</p>
