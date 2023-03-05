25388aff6f0ec60e2f7cf291711f2dd137b2a32d
Stefan Breunig <stefan-github@yrden.de>
2023-03-05 18:47:38 +0100

    update Alpine

--- lib/tool_versions.ex
+++ lib/tool_versions.ex
@@ -1,6 +1,6 @@
 defmodule ToolVersions do
   @path Path.join([__DIR__, "..", ".tool-versions"])
-  @extra_versions %{alpine: "3.16.0"}
+  @extra_versions %{alpine: "3.17.0"}
 
   @doc ~S"""
   Parses an asdf .tool-versions formatted string, and returns a map with atom keys
