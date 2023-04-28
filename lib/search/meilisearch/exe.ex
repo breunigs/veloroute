cfa446e6c4a5255107e8ce1253bd794378d999fd
Stefan Breunig <stefan-github@yrden.de>
2023-04-28 18:03:57 +0200

    bump meilisearch

--- lib/search/meilisearch/exe.ex
+++ lib/search/meilisearch/exe.ex
@@ -14,7 +14,7 @@ defmodule Search.Meilisearch.Exe do
       "aarch64" <> _ -> :aarch64
     end
 
-  @version "v1.0.2"
+  @version "v1.1.1"
   filename = "meilisearch-#{os}-#{arch}"
   @url "https://github.com/meilisearch/meilisearch/releases/download/#{@version}/#{filename}"
   @exe "#{filename}-#{@version}"
