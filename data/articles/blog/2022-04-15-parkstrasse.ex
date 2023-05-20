a167c1eb875405818206b9221ca00cd7db772562
Stefan Breunig <stefan-github@yrden.de>
2023-05-20 19:23:04 +0200

    updates

--- data/articles/blog/2022-04-15-parkstrasse.ex
+++ data/articles/blog/2022-04-15-parkstrasse.ex
@@ -3,34 +3,19 @@ defmodule Data.Article.Blog.Parkstrasse do
 
   def created_at(), do: ~D[2022-04-15]
 
-  def title(), do: "Parkstraße (Alltagsroute 1 und 14)"
-  def summary(), do: "Kanalsanierung erfordert Umleitung Richtung Innenstadt/Richtung Süden"
+  def title(), do: "Parkstraße / Groß Flottbeker Straße (Alltagsroute 1 und 14)"
+  def summary(), do: "Baustelle Fernwärmeleitung"
 
   def type(), do: :construction
 
   def tags(), do: ["1", "14"]
 
-  def tracks(),
-    do: [
-      %Video.Track{
-        renderer: 3,
-        group: "Parkstraße",
-        direction: :forward,
-        from: "Waitzstraße",
-        to: "Jungmannstraße",
-        parent_ref: __MODULE__,
-        text: "Umleitung Veloroute 14 Richtung Innenstadt",
-        videos: [
-          {"2022-05-15-13im-14gg/GX013956", "00:00:49.016", "00:01:20.280"},
-          {"2022-05-15-13im-14gg/GX013956", "00:01:23.250", "00:01:34.761"}
-        ]
-      }
-    ]
+  def tracks(),    do: []
 
   def text(assigns) do
     ~H"""
     <h4>Baustelle</h4>
-    <p>Die Baustelle ist eine Einbahnstraße Richtung Norden. Teilweise sind die Fußwege frei auf denen geschoben werden kann. In Richtung Innenstadt bzw. Süden ist eine Einbahnstraße eingerichtet.</p>
+    <p>Bau einer Fernwärmeleitung. Die Umleitungen sind ausgeschildert.</p>
     """
   end
 end
