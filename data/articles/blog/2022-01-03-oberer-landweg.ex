4cfc4a3a398fdf57836d7e51bc7bccfff95b931a
Stefan Breunig <stefan-github@yrden.de>
2023-10-25 17:10:12 +0200

    updates

--- data/articles/blog/2022-01-03-oberer-landweg.ex
+++ data/articles/blog/2022-01-03-oberer-landweg.ex
@@ -2,6 +2,7 @@ defmodule Data.Article.Blog.ObererLandweg do
   use Article.Default
 
   def created_at(), do: ~D[2022-01-03]
+  def updated_at(), do: ~D[2023-10-25]
 
   def title(), do: "Oberer Landweg (Alltagsroute 9, RSW Geesthacht)"
 
@@ -12,12 +13,15 @@ defmodule Data.Article.Blog.ObererLandweg do
   def type(), do: :construction
 
   def summary(),
-    do: "Planung 2022, Umbau bis 2023"
+    do:
+      "Beidseitig Zweirichtungsradwege unter der S-Bahn und auf der gesamten Westseite. Fuß- und Radverkehr bekommen mehr Platz als heute. Klarere Querungsmöglichkeiten und meist eigene Radampeln."
 
   def tags(), do: ["9", "rsw-geesthacht", "rsw"]
 
   def links(_assigns) do
     [
+      {"Lageplan und Erläuterungsbericht", "Oktober 2023",
+       "https://fragdenstaat.de/anfrage/oberer-landweg/"},
       {"Zeitungsartikel zum Umbaustart", "Juli 2023",
        "https://www.abendblatt.de/hamburg/bergedorf/article239022931/Oberer-Landweg-Neun-Monate-Baustelle-fuer-die-Veloroute.html"},
       {"Baustellenkoordination", "Januar 2023",
@@ -31,11 +35,35 @@ defmodule Data.Article.Blog.ObererLandweg do
 
   def text(assigns) do
     ~H"""
-    <h4>Alter Zustand</h4>
-    <p>Im Oberen Landweg gibt es für den Radverkehr <.ref>Hochbordradwege</.ref>. Stellenweise ist der Radverkehr in beide Richtungen vorgesehen, was aber häufig nur schlecht zu erkennen ist. Bei der Bahnunterführung müssen sich Fuß- und Radverkehr irgendwie arrangieren.</p>
+    <h4>Stand Oktober 2023</h4>
+    <p>Lageplan und Erläuterungen seitens der Stadt wurden veröffentlicht. Danke an den hartnäckigen Nachfrager.</p>
 
-    <h4>Vorhaben</h4>
-    <p>Laut Haushaltsentwurf 2022 befindet sich der Abschnitt zur Zeit in Planung und könnte ab 2023 umgesetzt werden.</p>
+    <h4>Alter Zustand</h4>
+    <p>Im <.v bounds="10.182936,53.485552,10.187685,53.491083" lon={10.186129} lat={53.488292} dir="forward" ref="9" highlight="Oberer Landweg">Oberen Landweg</.v> gibt es für den Radverkehr <.ref>Hochbordradwege</.ref>. Stellenweise ist der Radverkehr in beide Richtungen vorgesehen, was aber häufig nur schlecht zu erkennen ist. Bei der <.v bounds="10.182936,53.485552,10.187685,53.491083" lon={10.185727} lat={53.487419} dir="forward" ref="rsw-geesthacht">Bahnunterführung</.v> müssen sich Fuß- und Radverkehr irgendwie arrangieren.</p>
+
+    <h4>Planung</h4>
+    <h5>Entlang der Veloroute</h5>
+    <p>Auf der <.v bounds="10.182936,53.485552,10.187685,53.491083" lon={10.186288} lat={53.489815} dir="backward" ref="9" highlight="Oberer Landweg">Westseite des Oberen Landweg</.v> ist ein Zweirichtungsradweg im Hochbord mit 2,50m bis 3,50m Breite vorgesehen. Wie heute endet die Zweirichtungs-Lösung am <.v bounds="10.182936,53.485552,10.187685,53.491083" lon={10.186964} lat={53.490757} dir="backward" ref="9">Billwerder Billdeich</.v>. Eine Weiterfahrt in Richtung <.v bounds="10.182936,53.485552,10.187685,53.491083" lon={10.187323} lat={53.490534} dir="forward" ref="9">Kurt-A.-Körber-Chaussee</.v> ist nicht vorgesehen, was nicht rechtzeitig ersichtlich gemacht wird.</p>
+
+    <p>Auf der <.v bounds="10.182936,53.485552,10.187685,53.491083" lon={10.186129} lat={53.488292} dir="forward" ref="9" highlight="Oberer Landweg">Ostseite</.v> ist ist die Fahrt nur nach Norden erlaubt. Der Radweg wird als <.ref name="Hochbordradweg">Hochbord</.ref> (2,0m breit), im <.ref name="Kopenhagener Radweg">Kopenhagener Stil</.ref> (2,70m), und als <.ref>Protected-Bike-Lane</.ref> (2,50m) ausgeführt. Wegen der vielen Einfahrten sind nur kurze Abschnitte vom KFZ-Verkehr durch einen Bordstein getrennt.</p>
+
+    <h5>Entlang des Radschnellwegs</h5>
+    <p>Auf der <.v bounds="10.182936,53.485552,10.187685,53.491083" lon={10.185511} lat={53.487778} dir="backward" ref="9">Westseite</.v> setzt sich der oben genante Zweirichtungsradweg fort. Allerdings wird er als <.ref>Kopenhagener Radweg</.ref> geführt.</p>
+
+    <p>Auf der <.v bounds="10.182936,53.485552,10.187685,53.491083" lon={10.185727} lat={53.487419} dir="forward" ref="rsw-geesthacht">Ostseite</.v> ist auch ein Zweirichtungsrad im Kopenhagener Stil geplant. Er wird mit 3,0m etwas breiter, weil diese Seite für den Radschnellweg vorgesehen ist. Rad- und Gehweg münden direkt in den <.v bounds="10.176846,53.485552,10.187685,53.491083" lon={10.185554} lat={53.487008} dir="forward" ref="rsw-geesthacht">Wehrdeich</.v>. Dort wird dann im <.ref>Mischverkehr</.ref> gefahren.</p>
+
+    <h5>Querungsmöglichkeiten</h5>
+    <p>An der Kreuzung <.m bounds="10.183486,53.485887,10.186654,53.48768">Nettelnburger Landweg</.m> kann man von West nach Ost mittels eigener Ampeln wechseln. In der Gegenrichtung erfolgt dies gemeinsam mit den Autos auf geteilten Spuren.</p>
+
+    <p>Am <.v bounds="10.183509,53.486635,10.187781,53.488753" lon={10.185296} lat={53.487676} dir="forward" ref="9">Karl-Heinz-Rissmann-Weg</.v> darf in beide Richtungen gequert werden. Auf der Westseite ist diese Fahrtrichtung gegenüber dem Nord/Süd Radweg bevorrechtigt. Dies wird durch die in den Niederlanden gebräuchlichen „Haifischzähne“ verdeutlicht. Zu Fuß müssen an dieser Stelle die beiden Zweirichtungsradwege gequert werden, die deswegen rot hervorgehoben werden.</p>
+
+    <p>Nördlich des <.v bounds="10.185049,53.488366,10.187419,53.490041" lon={10.186445} lat={53.489187} dir="forward" ref="9">Ladenbeker Furtweg</.v> gibt es eine Möglichkeit die Straßenseite zu wechseln. Wer direkt aus dem Furtweg kommt, muss also zweimal die Straßenseite wechseln um der Veloroute in Richtung Bergedorf zu folgen.</p>
+
+    <h5>Sonstiges</h5>
+    <p>Als Oberflächen sind rotes Betonsteinpflaster und Asphalt vorgesehen. Am <.v bounds="10.183509,53.486635,10.187781,53.488753" lon={10.185296} lat={53.487676} dir="forward" ref="9">Karl-Heinz-Rissmann-Weg</.v> soll ein Fahrrad-Dauergrün in den Nebenzeiten ausprobiert werden. Auch die Fußwege unter der S-Bahn-Brücke werden breiter.</p>
+
+    <h4>Meinung</h4>
+    <p>Auf den ersten Blick eine gelungene Planung. Die Radwege sind breit, Seitenwechsel ist leicht möglich, aber je nach Ziel optional. Die erlaubten Fahrtrichtungen sind eindeutig erkennbar. Die Praxis wird zeigen müssen ob die Ampeln brauchbar funktionieren. Gerade die Westseite am Karl-Heinz-Rissmann-Weg ist spannend, da hier zwei Zweirichtungsradwege und der Fußverkehr einander kreuzen.</p>
 
     <h4>Quelle</h4>
     <.structured_links ref={@ref}/>
---
+++ data/articles/blog/2023-10-25-martinistrasse-ost.ex
@@ -0,0 +1,37 @@
+defmodule Data.Article.Blog.MartinistrasseOst do
+  use Article.Default
+
+  def title(), do: "Martinistraße Ost (Bezirksroute Nord 6)"
+
+  def start(), do: ~d[2024Q2]
+
+  def type(), do: :planned
+
+  def summary(),
+    do: "Schmälere Fahrbahn, breitere Gehwege, erstmals Anschließbügel für Fahrräder."
+
+  def tags(), do: ["br-nord-n6"]
+
+  def links(_assigns) do
+    [
+      {"Lageplan und Erläuterungsbericht", "Oktober 2023",
+       "https://sitzungsdienst-hamburg-nord.hamburg.de/bi/vo020.asp?VOLFDNR=1013221"}
+    ]
+  end
+
+  def text(assigns) do
+    ~H"""
+    <h4>Alter Zustand</h4>
+    <p>Es gibt keine eigenen Radwege; die Fahrspuren sind im <.ref>Mischverkehr</.ref> mit den KFZ zu teilen.</p>
+
+    <h4>Planung</h4>
+    <p>Die Führungsform bleibt Mischverkehr. Die Fahrbahn wird zu Gunsten der Fußwege verschmälert. Erstmals kommen auch Anschließbügel für Fahrräder hinzu. Die Kreuzungen sind nicht teil dieser Planung.</p>
+
+    <h4>Meinung</h4>
+    <p>Die Verbesserungen für den Radverkehr beschränken sich auf die Anschließbügel. Die fehlenden eigenen Wege werden sich vor allem bei Stau bemerkbar machen, wenn man nicht mehr vorbeifahren kann. Auch für junge und neue Radfahrer bleibt der Abschnitt mit viel Busverkehr eine Problemstelle.</p>
+
+    <h4>Quelle</h4>
+    <.structured_links ref={@ref}/>
+    """
+  end
+end
