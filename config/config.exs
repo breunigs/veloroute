829beb9748730674a5dea6cf1b63f50a2b3fcd01
Stefan Breunig <stefan-github@yrden.de>
2023-12-17 14:36:03 +0100

    fix 4xx errors becoming 500s

--- config/config.exs
+++ config/config.exs
@@ -21,7 +21,10 @@ if File.exists?(creds_path),
 config :veloroute, VelorouteWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: PhoenixCredentials.secret_key_base(),
-  render_errors: [view: VelorouteWeb.ErrorView, accepts: ~w(html json)],
+  render_errors: [
+    formats: [html: VelorouteWeb.ErrorHTML],
+    layout: false
+  ],
   live_view: [signing_salt: PhoenixCredentials.live_view_signing_salt()]
 
 config :veloroute,
