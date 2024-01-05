6353e708978f32bc7b53a757fbe6009ac5edbe5b
Stefan Breunig <stefan-github@yrden.de>
2024-01-05 18:33:01 +0100

    non-zero exit code if static map render image build fails

--- lib/mix/tasks/velo.map.static_renderer.ex
+++ lib/mix/tasks/velo.map.static_renderer.ex
@@ -5,11 +5,17 @@ defmodule Mix.Tasks.Velo.Map.StaticRenderer do
   @requirements ["app.start"]
   @shortdoc "Builds the static renderer executable"
   def run(_) do
-    Logger.info("building static renderer")
+    if Basemap.Static.Exe.stale?() do
+      Logger.info("building static renderer")
 
-    case Basemap.Static.Exe.ensure() do
-      {:error, reason} -> Logger.error(reason)
-      :ok -> :ok
+      case Basemap.Static.Exe.ensure() do
+        {:error, reason} ->
+          Logger.error(reason)
+          exit({:shutdown, 1})
+
+        :ok ->
+          :ok
+      end
     end
   end
 end
