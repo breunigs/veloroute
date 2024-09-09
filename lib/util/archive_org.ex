b8f2dddbea95bb10d5efbcfa303d764d9d0a4c2f
Stefan Breunig <307954+breunigs@users.noreply.github.com>
2024-09-09 09:03:44 +0200

    allow archive.org'd redirects if no proper page is available

--- lib/util/archive_org.ex
+++ lib/util/archive_org.ex
@@ -36,21 +36,31 @@ defmodule Util.ArchiveOrg do
   end
 
   defp latest_successful(body) do
-    body
-    |> Enum.map(fn [url_key, timestamp, original, mimetype, status_code, digest, length] ->
-      %{
-        url_key: url_key,
-        timestamp: timestamp,
-        original: original,
-        mimetype: mimetype,
-        status_code: status_code,
-        digest: digest,
-        length: length
-      }
-    end)
-    |> Enum.reject(fn entry -> entry.status_code != "200" end)
-    |> Enum.sort_by(& &1.timestamp)
-    |> List.last()
-    |> Kernel.||({:error, "search for this URL turned up no results"})
+    candidates =
+      body
+      |> Enum.map(fn [url_key, timestamp, original, mimetype, status_code, digest, length] ->
+        %{
+          url_key: url_key,
+          timestamp: timestamp,
+          original: original,
+          mimetype: mimetype,
+          status_code: status_code,
+          digest: digest,
+          length: length
+        }
+      end)
+      |> Enum.sort_by(& &1.timestamp)
+
+    real_page =
+      candidates
+      |> Enum.reject(fn entry -> entry.status_code != "200" end)
+      |> List.last()
+
+    redirect =
+      candidates
+      |> Enum.reject(fn entry -> !(entry.status_code in ["301", "302"]) end)
+      |> List.last()
+
+    real_page || redirect || {:error, "search for this URL turned up no results"}
   end
 end
