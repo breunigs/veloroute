d57946f1952e83a3e5100d444ecba1f4f18381d6
Stefan Breunig <307954+breunigs@users.noreply.github.com>
2025-08-15 18:59:13 +0200

    ferdinandstor

--- data/articles/blog/2021-07-30-ferdinandstor.ex
+++ data/articles/blog/2021-07-30-ferdinandstor.ex
@@ -1,33 +1,49 @@
 defmodule Data.Article.Blog.Ferdinandstor do
   use Article.Default
 
-  def created_at(), do: ~D[2021-07-30]
+  def updated_at(), do: ~D[2025-08-15]
 
   def title(), do: "Ferdinandstor (Radrouten 2 und 6, Freizeitrouten 2 und 3)"
 
+  def summary(),
+    do:
+      "Alsterseite: 4,0m breite Protected-Bike-Lane unter der Brücke. Seite Kunsthalle: 2,5m Hochbordradweg. Anschlüsse bleiben wie bisher."
+
   def start(), do: ~d[2021-06-14]
-  def stop(), do: ~d[2025-10-31]
+  def stop(), do: ~d[2026Q3]
   def construction_site_id_hh(), do: [16775, 718_592]
 
   def type(), do: :construction
 
-  def tags(), do: ["radroute-2", "radroute-6", "5", "6", "FR2", "FR3"]
+  def tags(), do: ["radroute-6", "radroute-2", "5", "6", "FR2", "FR3"]
 
   def tracks(), do: []
 
+  def map_image do
+    {name(), [{"LSBG Hamburg", "https://lsbg.hamburg.de/ueber-uns/kontakt"}]}
+  end
+
   def links(_assigns) do
-    []
+    [
+      {"Straßenplanung: Karte und Bericht", ~d[2025-08],
+       "https://fragdenstaat.de/anfrage/ferdinandstor-und-an-der-alster-anckelmannsplatz/#nachricht-1027031"},
+      {"Baustelleninfo der Bahn",
+       "https://www.eisenbahnbruecken-ehm.de/ferdinandstor-an-der-alster.html"}
+    ]
   end
 
   def text(assigns) do
     ~H"""
-    <h4>Baustelle</h4>
-    <p>Die Deutsche Bahn erneuert die Bahnbrücken um das Ferdinandstor, sodass Rad- und Fußverkehr hier ortsnah umgeleitet werden. Bis auf einige rund 14-tägige Vollsperrungen bleibt die Unterführung aber in beide Richtungen offen. Die Zeiträume für diese stehen noch nicht genau fest, die Bahn gibt hier Oktober 2022, Juli/August 2023, sowie 2024 und 2025 an.</p>
+    <h4>Alter Zustand</h4>
+    <p>Beidseitig waren <.ref>Hochbordradwege</.ref> vorhanden. Seit Sommer 2021 erneuert die Deutsche Bahn die Brücke, sodass sich die genauer Führung immer mal wieder ändert.</p>
+
+    <.h4_planning ref={@ref} checked={@show_map_image}/>
+    <p>Auf der <.v bounds="10.000008,53.555662,10.003289,53.557319" lon={10.001107} lat={53.555802} dir="backward" ref="radroute-2">Seite der Kunsthalle</.v> wird ein 2,5m breiter Hochbordradweg neben einem 2,5m breiten Gehweg angelegt. Zum Schutz vor KFZ werden Metallbügel aufgestellt.</p>
+
+    <p>Auf <.v bounds="10.000008,53.555662,10.003289,53.557319" lon={10.002083} lat={53.557174} dir="forward" ref="radroute-2">der Alsterseite</.v> weitet sich der schmale Hochbordradweg zu einer 4,0m breiten <.ref>Protected-Bike-Lane</.ref> unter der Brücke auf. Der Radweg verläuft rechts der Brückenpfeiler. Die <.v bounds="9.999839,53.555578,10.002548,53.55692" lon={10.000689} lat={53.556429} dir="forward" ref="radroute-2">Fahrradweiche über den KFZ-Rechtsabbieger</.v> bleibt in etwa wie heute. Der Gehweg wird hier rund 3,25m breit.</p>
 
     <h4>Quelle</h4>
-    <p>
-    <.a href="https://bauprojekte.deutschebahn.com/p/hamburg-ferdinandstor/pdf">Baustelleninfo der Bahn</.a>
-    </p>
+    <.structured_links ref={@ref}/>
     """
   end
 end
