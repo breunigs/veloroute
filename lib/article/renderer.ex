2672b13111a063574d8b6f2486f27989ee7874ab
Stefan Breunig <307954+breunigs@users.noreply.github.com>
2025-12-06 17:00:07 +0100

    remove hacky rendering error catcher

--- lib/article/renderer.ex
+++ lib/article/renderer.ex
@@ -15,31 +15,29 @@ defmodule Article.Renderer do
   attr :device_os, :string, default: ""
 
   def render(%{ref: art} = assigns) when is_module(art) do
-    try_render(assigns, fn ->
-      body = art.text(assigns)
-      has_title = assigns.ref.title() != ""
+    body = art.text(assigns)
+    has_title = assigns.ref.title() != ""
 
-      assigns =
-        assign(assigns, %{
-          body: body,
-          insert_h3: !has_header?(body) && has_title,
-          image_url: if(art.microdata?(), do: Article.Decorators.start_image_path(art))
-        })
+    assigns =
+      assign(assigns, %{
+        body: body,
+        insert_h3: !has_header?(body) && has_title,
+        image_url: if(art.microdata?(), do: Article.Decorators.start_image_path(art))
+      })
 
-      ~H"""
-        <article {@ref.microdata(:wrapper)}>
-          <h3 {@ref.microdata(:title)} :if={@insert_h3}><%= @ref.title() %></h3>
-          <Components.TagHelpers.construction_duration_header ref={@ref}/>
+    ~H"""
+      <article {@ref.microdata(:wrapper)}>
+        <h3 {@ref.microdata(:title)} :if={@insert_h3}><%= @ref.title() %></h3>
+        <Components.TagHelpers.construction_duration_header ref={@ref}/>
 
-          <%= @body %>
-          <Components.TagHelpers.construction_duration_paragraph ref={@ref} lang={@lang}/>
-          <Components.TagHelpers.article_updated_at ref={@ref} lang={@lang}/>
-          <meta itemprop="image" content={@image_url} :if={@image_url}/>
-        </article>
+        <%= @body %>
+        <Components.TagHelpers.construction_duration_paragraph ref={@ref} lang={@lang}/>
+        <Components.TagHelpers.article_updated_at ref={@ref} lang={@lang}/>
+        <meta itemprop="image" content={@image_url} :if={@image_url}/>
+      </article>
 
-        <Components.RelatedArticlesHelper.related_articles ref={@ref} lang={@lang}/>
-      """
-    end)
+      <Components.RelatedArticlesHelper.related_articles ref={@ref} lang={@lang}/>
+    """
   end
 
   defp has_header?(body) do
@@ -48,31 +46,4 @@ defmodule Article.Renderer do
     String.starts_with?(start, "<h3") ||
       (String.starts_with?(start, "<!--") && String.contains?(start, "<h3"))
   end
-
-  # only ignore the errors in development to avoid reloading the page. Always
-  # rendering in other environments should yield errors during (integration)
-  # testing.
-  defp try_render(assigns, callback, env \\ Application.get_env(:veloroute, :env))
-
-  defp try_render(%{ref: art} = assigns, callback, :dev) do
-    try do
-      _ = Article.Decorators.html(art, assigns)
-      callback.()
-    rescue
-      e ->
-        pretty = Exception.format(:error, e, __STACKTRACE__)
-        Logger.error(pretty)
-
-        assigns = assign(assigns, pretty: pretty)
-
-        ~H"""
-        <h4>failed to render article</h4>
-        <tt><%= @ref %></tt>
-        <br><br>
-        <pre><%= @pretty %></pre>
-        """
-    end
-  end
-
-  defp try_render(_assigns, callback, _env), do: callback.()
 end
