5abac9890c1d35347ab3f0888ef470718be84bef
Stefan Breunig <307954+breunigs@users.noreply.github.com>
2026-06-20 11:58:42 +0200

    updates

--- data/articles/blog/2018-09-29-1-blankeneser-friedhof.ex
+++ data/articles/blog/2018-09-29-1-blankeneser-friedhof.ex
@@ -20,6 +20,8 @@ defmodule Data.Article.Blog.BlankeneserFriedhof do
 
   def links(_assigns) do
     [
+      {"Präsentation u.a. auch zum Sorgfeld", ~d[2022-03],
+       "https://sitzungsdienst-altona.hamburg.de/bi/to020.asp?TOLFDNR=1037758"},
       {"Zeitungsartikel",
        "https://www.abendblatt.de/hamburg/article234200157/stadtplanung-abendblatt-serie-altona-hamburg-stadtentwicklung-bauvorhaben-planung-ottensen.html"}
     ]
@@ -31,7 +33,7 @@ defmodule Data.Article.Blog.BlankeneserFriedhof do
     <p>Am <.m bounds="9.796375,53.572692,9.805467,53.577368">Friedhof Blankenese</.m> gibt es an beiden Zugangsstellen sehr enge Drängelgitter. Diese sind bereits mit einem normalen Fahrrad kaum zu passieren, mit Anhänger oder Lastenrad kommt man vermutlich überhaupt nicht durch. Allgemein erscheint die Führung durch den Friedhof fragwürdig, liegt doch der <.m bounds="9.793264,53.56325,9.812449,53.586809">Sülldorfer Kirchenweg</.m> knapp daneben. Vermutlich traute man sich nicht den KFZ Verkehr dort einzudämmen.</p>
 
     <h4>Vorhaben</h4>
-    <p>Laut einem Zeitungsartikel soll der Weg in diesem Abschnitt zu einem getrennten Rad- und Fußweg umgebaut werden.</p>
+    <p>Laut einem Zeitungsartikel soll der Weg in diesem Abschnitt zu einem getrennten Rad- und Fußweg umgebaut werden. In einer Präsentation des Bezirks findet sich ein Querschnitt der einen 2,45m breiten Gehweg neben einem 3,0m breiten Zweirichtungsradweg vorsieht.</p>
 
     <h4>Quelle</h4>
     <.structured_links ref={@ref}/>
