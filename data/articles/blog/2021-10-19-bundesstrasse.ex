1ef544e938c5bcadf680bb0e0fd5cdcde910bf64
Stefan Breunig <307954+breunigs@users.noreply.github.com>
2026-08-03 19:07:41 +0200

    updates

--- data/articles/blog/2021-10-19-bundesstrasse.ex
+++ data/articles/blog/2021-10-19-bundesstrasse.ex
@@ -3,10 +3,12 @@ defmodule Data.Article.Blog.Bundesstrasse do
 
   def title(), do: "Bundesstraße (Verbindungsbahn bis Rentzelstraße)"
 
-  def summary(), do: "Bundesstraße soll zur Fahrradstraße umgebaut werden."
+  def updated_at(), do: ~D[2026-08-03]
+
+  def summary(), do: "Bundesstraße soll zur Fahrradstraße mit KFZ-Einbahnstraße umgebaut werden."
 
   def type(), do: :planned
-  def start(), do: ~d[2026Q3]
+  def start(), do: ~d[2027]
 
   def tags(), do: ["Bezirksroute B", "Eimsbüttel"]
 
@@ -48,6 +50,15 @@ defmodule Data.Article.Blog.Bundesstrasse do
       }
     ]
 
+  def map_image do
+    {name(),
+     [
+       {"melchior + wittpohl", "https://mplusw.de/kontakt-anfrage/"},
+       {"Bezirksamt Eimsbüttel – Fachamt Management des öffentlichen Raumes",
+        "https://www.hamburg.de/politik-und-verwaltung/bezirke/bezirksamt-eimsbuettel/ihr-bezirksamt/fachamt-management-des-oeffentlichen-raumes-58482"}
+     ]}
+  end
+
   def links(_assigns) do
     [
       {"Zeitungsartikel zu möglichem Bautermin", ~d[2025-12],
@@ -55,20 +66,26 @@ defmodule Data.Article.Blog.Bundesstrasse do
       {"Geplanter Bautermine", "November 2023",
        "https://sitzungsdienst-eimsbuettel.hamburg.de/bi/to020.asp?TOLFDNR=1030471"},
       {"Unterlagen des 1. Entwurfs",
-       "https://sitzungsdienst-eimsbuettel.hamburg.de/bi/vo020.asp?VOLFDNR=1007009"}
+       "https://sitzungsdienst-eimsbuettel.hamburg.de/bi/vo020.asp?VOLFDNR=1007009"},
+      {"Unterlagen des 2. Entwurfs", ~d[2026-07],
+       "https://sitzungsdienst-eimsbuettel.hamburg.de/bi/vo020.asp?VOLFDNR=1011898"}
     ]
   end
 
   def text(assigns) do
     ~H"""
-    <h4>Alter Zustand</h4> <p>Im Planungsgebiet gibt es beidseitig schmale <.ref>Hochbordradwege</.ref>, die durch angekettete Fahrräder teils noch enger werden. Sie müssen nicht benutzt werden.</p>
-    <h4>Planung</h4> <p>Die Straße soll zu einer Fahrradstraße umgebaut werden. Die Fahrbahn soll künftig dann nur noch 5,50m breit sein, wobei am <.m bounds="9.979352,53.564563,9.981155,53.565757">Durchschnitt</.m> die Fahrbahn auf 4,0m verengt werden soll. Die Gehwegüberfahrten bleiben weiterhin erhalten.</p>
-    <p>An der <.m bounds="9.981335,53.563922,9.983163,53.564482">Verbindungsbahn</.m> sollen KFZ künftig nicht mehr einbiegen dürfen, die Straße wird aber nicht zu einer Einbahnstraße. Eine komplette Durchfahrtssperre für KFZ wurde von Feuerwehr und Rettungsdiensten abgelehnt.</p>
-    <p>Die alten Hochbordradwege werden den Fußwegen zugeschlagen und die KFZ-Parkplätze neu sortiert. Es sollen zahlreiche neue Radanlehnbügel aufgestellt werden; von heute 44 auf künftig 121.</p>
-    <p>Ob die Fahrradstraße eingerichtet werden darf ist noch offen, da eine Verkehrszählung abgewartet wird. Nur wenn dort der Radverkehrsanteil hoch genug ausfällt, sind Fahrradstraßen gesetzlich möglich. Sollte dies nicht machbar sein, würde der Abschnitt in einem ähnlichen Stil zu einer Tempo-30-Zone umgebaut.</p>
-    <p>Der Bezirk plant zu diesem Entwurf noch eine Online-Umfrage einzurichten.</p>
-    <h4>Meinung</h4> <p>Es ist schade das man die KFZ-Sackgasse so schnell verworfen hat, statt die Möglichkeiten genauer auszuloten. Mit Automatikpollern oder mit Blaulicht über den Radweg entstünden für die Rettungskräfte auch keine Umwege.</p>
-    <p>Die halbseitige Sperrung wird den KFZ-Verkehr und dessen Lärm vermutlich trotzdem etwas verringern. Das muss sie aber auch, wenn der Radverkehr auf der Fahrbahn fahren soll.</p>
+    <h4>Alter Zustand</h4>
+    <p>Beidseitig gibt es <.ref>Hochbordradwege</.ref> mit 0,8m bis 1,5m Breite. Wer mag, darf auch auf der <.ref>Fahrbahn</.ref> radeln.</p>
+
+    <.h4_planning ref={@ref} checked={@show_map_image}/>
+    <p>Die Bundesstraße soll zu einer Fahrradstraße („Anlieger frei“) umgebaut werden. Gleichzeitig wird sie <.v bounds="9.977069,53.564439,9.981689,53.566484" lon={9.979515} lat={53.565638} dir="backward" ref={Bundesstrasse}>ab der Tankstelle</.v> für den KFZ-Verkehr zur Einbahnstraße Richtung Dammtor. <.v bounds="9.980331,53.56376,9.984305,53.565212" lon={9.982919} lat={53.5642} dir="backward" ref={Radroute2}>An der Verbindungsbahn</.v> wird der frei werdende Platz für eine <.ref>Protected-Bike-Lane</.ref> benutzt.</p>
+
+    <p>Die Fahrbahn wird künftig 5,00m breit sein. Am <.v bounds="9.978744,53.563928,9.98174,53.566308" lon={9.980621} lat={53.565247} dir="forward" ref={Bundesstrasse}>Durchschnitt</.v> wird die Fahrbahn auf 4,00m eingeengt. Die alten Hochbordradwege werden den Gehwegen zugeschlagen.</p>
+
+    <h4>Meinung</h4>
+    <p>Die KFZ-Einbahnstraße ist eine Verbesserung gegenüber dem ersten Entwurf und dürfte den Schleichverkehr reduzieren. Dank der Trennung zum KFZ-Verkehr an der Verbindungsbahn wird diese Stelle angenehmer zu befahren.</p>
+
+    <p>Anders als im ersten Entwurf behält man die Schrägparkplätze. Man verringert damit die Verkehrssicherheit für den Radverkehr, um den Autofahrern mehr Bequemlichkeit zu ermöglichen. Die Schuld liegt hier in den Vorgaben der Politik. Das Planungsbüro hat noch das Beste herausgeholt, um den Anwohnerwünschen zu entsprechen.</p>
 
     <h4>Quelle</h4>
     <.structured_links ref={@ref}/>
